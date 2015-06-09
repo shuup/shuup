e49dc16bb2014126b7a24c57aa22ad4f18c892b7
Tuomas Suutari <tuomas.suutari@andersinno.fi>
2015-06-09 09:54:13 +0300

    Fix formatting of copyright headers in js files
    Add the missing space character to beginning of lines.
    
    Refs SHOOP-978

--- shoop/notify/static_src/script-editor.js
+++ shoop/notify/static_src/script-editor.js
@@ -1,10 +1,10 @@
 /**
-* This file is part of Shoop.
-*
-* Copyright (c) 2012-2015, Shoop Ltd. All rights reserved.
-*
-* This source code is licensed under the AGPLv3 license found in the
-* LICENSE file in the root directory of this source tree.
+ * This file is part of Shoop.
+ *
+ * Copyright (c) 2012-2015, Shoop Ltd. All rights reserved.
+ *
+ * This source code is licensed under the AGPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 var style = require("style!css!autoprefixer!less!./script-editor.less");
 
