557a096ddf672846b28523134ef54966acabe535
Aarni Koskela <aarni.koskela@shoop.io>
2015-08-03 17:41:35 +0300

    Add admin UI for product sales units.
    Refs Github issue #43
    Refs SHOOP-1037

---
+++ shoop/admin/modules/sales_units/views/list.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# This file is part of Shoop.
+#
+# Copyright (c) 2012-2015, Shoop Ltd. All rights reserved.
+#
+# This source code is licensed under the AGPLv3 license found in the
+# LICENSE file in the root directory of this source tree.
+from __future__ import unicode_literals
+from shoop.admin.utils.picotable import Column, TextFilter
+from shoop.admin.utils.views import PicotableListView
+from shoop.core.models import SalesUnit
+from django.utils.translation import ugettext_lazy as _
+
+
+class SalesUnitListView(PicotableListView):
+    model = SalesUnit
+    columns = [
+        Column("name", _(u"Name"), sort_field="translations__name", display="name", filter_config=TextFilter(
+            filter_field="translations__name",
+            placeholder=_("Filter by name...")
+        )),
+        Column("short_name", _(u"Short Name"), sort_field="translations__short_name", display="short_name"),
+        Column("decimals", _(u"Allowed decimals")),
+    ]
+
+    def get_queryset(self):
+        return SalesUnit.objects.all()
