86979add49a342ae9ad36d67fe8a34c813a0126e
Aarni Koskela <aarni.koskela@shoop.io>
2015-08-03 15:32:10 +0300

    Refactor media widget into more generic browse widget
    Refs SHOOP-1038

--- shoop/admin/forms/widgets.py
+++ shoop/admin/forms/widgets.py
@@ -1,34 +1,59 @@
 # -*- coding: utf-8 -*-
 from django.forms import HiddenInput, Widget
 from django.utils.encoding import force_text
-from django.utils.safestring import mark_safe
 from django.utils.html import escape
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy as _
 from filer.models import File
+from shoop.admin.utils.urls import get_model_url, NoModelUrl
 
 
-class MediaChoiceWidget(Widget):
-    BROWSE_MARKUP = "<button class='media-browse btn btn-sm' type='button'><i class='fa fa-folder'></i> Browse</button>"
+class BasePopupChoiceWidget(Widget):
+    browse_kind = None
+
+    def get_browse_markup(self):
+        icon = "<i class='fa fa-folder'></i>"
+        return "<button class='browse-btn btn btn-info btn-sm' type='button'>%(icon)s %(text)s</button>" % {
+            "icon": icon,
+            "text": _("Browse")
+        }
 
     def render_text(self, obj):
+        url = getattr(obj, "url", None)
+        text = u"\u2014"
         if obj:
             text = force_text(obj)
-            url = obj.url
-        else:
-            text = u"\u2014"
-            url = "about:blank"
+            if not url:
+                try:
+                    url = get_model_url(obj)
+                except NoModelUrl:
+                    pass
+        if not url:
+            url = "#"
 
-        return mark_safe("<a class=\"media-text\" href=\"%(url)s\" target=\"_blank\">%(text)s</a>&nbsp;" % {
+        return mark_safe("<a class=\"browse-text\" href=\"%(url)s\" target=\"_blank\">%(text)s</a>&nbsp;" % {
             "text": escape(text),
             "url": escape(url),
         })
 
+    def get_object(self, value):
+        raise NotImplementedError("Not implemented")
+
     def render(self, name, value, attrs=None):
         if value:
-            obj = File.objects.get(pk=value)
+            obj = self.get_object(value)
         else:
             obj = None
         pk_input = HiddenInput().render(name, value, attrs)
         media_text = self.render_text(obj)
-        bits = "".join([pk_input, media_text, self.BROWSE_MARKUP])
-        return mark_safe("<div class='media-widget'>%s</div>" % bits)
+        bits = "".join([pk_input, media_text, self.get_browse_markup()])
+        return mark_safe("<div class='browse-widget' data-browse-kind='%(browse_kind)s'>%(bits)s</div>" % {
+            "browse_kind": self.browse_kind,
+            "bits": bits
+        })
 
+
+class MediaChoiceWidget(BasePopupChoiceWidget):
+    browse_kind = "media"
+    def get_object(self, value):
+        return File.objects.get(pk=value)
