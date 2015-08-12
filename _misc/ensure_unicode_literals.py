1b13bf4628f05399e3d395084f2d0544b73a7e7e
Aarni Koskela <aarni.koskela@shoop.io>
2015-08-12 15:40:21 +0300

    `ensure_unicode_literals`: % formatting also means unicode is required

--- _misc/ensure_unicode_literals.py
+++ _misc/ensure_unicode_literals.py
@@ -8,35 +8,64 @@
 import re
 import os
 import click
-from ast import parse, NodeVisitor
+from ast import parse, iter_fields, AST, BinOp, Mod
 from six import text_type
 
 encoding_comment_regexp = re.compile(r'^#.+coding[=:]\s*([-\w.]+).+$', re.MULTILINE | re.I)
 
 
-class StringVisitor(NodeVisitor):
+class XNodeVisitor(object):
+    def visit(self, node, parents=None):
+        method = 'visit_' + node.__class__.__name__
+        visitor = getattr(self, method, self.generic_visit)
+        return visitor(node=node, parents=parents)
+
+    def generic_visit(self, node, parents=None):
+        parents = (parents or []) + [node]
+        for field, value in iter_fields(node):
+            if isinstance(value, list):
+                for item in value:
+                    if isinstance(item, AST):
+                        self.visit(item, parents)
+            elif isinstance(value, AST):
+                self.visit(value, parents)
+
+
+class StringVisitor(XNodeVisitor):
     def __init__(self):
         self.texts = set()
+        self.formattees = set()
 
-    def visit_Str(self, node):  # noqa (N802)
+    def visit_Str(self, node, parents):  # noqa (N802)
         s = text_type(node.s)
-        if ("\n" in s or s.islower() or s.isupper()):  # Looks like a constant or docstring
+        is_being_formatted = (parents and isinstance(parents[-1], BinOp) and isinstance(parents[-1].op, Mod))
+        if is_being_formatted:
+            self.formattees.add(s)
             return
-        if " " in s.strip():  # Has spaces, that's texty
-            if "%" in s or not all(32 <= ord(c) < 127 for c in s):  # Has a formatting character or is non-ascii
-                self.texts.add(s)
-        self.generic_visit(node)
+        if not ("\n" in s or s.islower() or s.isupper()):  # Doesn't look like a constant or docstring
+            if " " in s.strip():  # Has spaces, that's texty
+                if "%" in s or not all(32 <= ord(c) < 127 for c in s):  # Has a formatting character or is non-ascii
+                    self.texts.add(s)
+
+    def get_stats(self):
+        stat_bits = []
+        if self.texts:
+            stat_bits.append("%d text-like strings" % len(self.texts))
+        if self.formattees:
+            stat_bits.append("%d formattee strings" % len(self.formattees))
+        return ", ".join(stat_bits)
+
+    def needs_fix(self):
+        return bool(self.texts or self.formattees)
 
 
 def process_file(path):
+    sv = StringVisitor()
     with open(path, "rb") as fp:
         source = fp.read()
-    if b"unicode_literals" in source:
-        return
-    tree = parse(source, path)
-    sv = StringVisitor()
-    sv.visit(tree)
-    return sv.texts
+    if b"unicode_literals" not in source:
+        sv.visit(parse(source, path))
+    return sv
 
 
 def fix_file(path):
@@ -85,9 +114,9 @@ def gather_files(dirnames, filenames):
 @click.option('--fix/--no-fix', default=False)
 def command(filenames, dirnames, fix):
     for filename in gather_files(dirnames, filenames):
-        results = process_file(filename)
-        if results:
-            print("%s: %d text-like strings but no unicode_literals" % (filename, len(results)))
+        visitor = process_file(filename)
+        if visitor.needs_fix():
+            print("%s: %s" % (filename, visitor.get_stats()))
             if fix:
                 print("Fixing: %s" % filename)
                 fix_file(filename)
