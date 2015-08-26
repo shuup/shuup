e8fa732e79b7e7ab8e9170fbce6db0131c0cdd36
Juha Kujala <juha.kujala@shoop.io>
2015-08-26 06:07:45 +0000

    Modify product page icons and actions dropdown menu
    * Change product images icon to camera icon
    * Group different categories in actions dropdown and add correct icons
    
    Refs SHOOP-1091

--- shoop/admin/modules/products/views/toolbars.py
+++ shoop/admin/modules/products/views/toolbars.py
@@ -10,7 +10,9 @@ from __future__ import unicode_literals
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext as _
 
-from shoop.admin.toolbar import DropdownActionButton, DropdownDivider, DropdownItem, Toolbar, get_default_edit_toolbar
+from shoop.admin.toolbar import (
+    DropdownActionButton, DropdownDivider, DropdownHeader, DropdownItem, Toolbar, get_default_edit_toolbar
+)
 from shoop.admin.utils.urls import get_model_url
 
 
@@ -38,11 +40,14 @@ class EditProductToolbar(Toolbar):
         )
         cross_sell_button = DropdownItem(
             text=_("Manage Cross-Selling"),
-            icon="fa fa-exchange",
+            icon="fa fa-random",
             url=reverse("shoop_admin:product.edit_cross_sell", kwargs={"pk": product.pk}),
         )
         menu_items = [
+            DropdownHeader(text=_("Media")),
             media_button,
+            DropdownDivider(),
+            DropdownHeader(text=_("Cross-Selling")),
             cross_sell_button
         ]
 
@@ -64,64 +69,69 @@ class EditProductToolbar(Toolbar):
         variation_url = reverse("shoop_admin:product.edit_variation", kwargs={"pk": product.pk})
         if variation_parent:
             yield DropdownDivider()
+            yield DropdownHeader(text=_("Variations"))
             yield DropdownItem(
                 text=_("Manage Variations"),
-                icon="fa fa-arrows-alt",
+                icon="fa fa-sitemap",
                 url=variation_url,
             )
             for child in product.variation_children.all():
                 yield DropdownItem(
                     text=_("Child: %s") % child,
-                    icon="fa fa-long-arrow-down",
+                    icon="fa fa-eye",
                     url=get_model_url(child),
                 )
         elif variation_child:
             yield DropdownDivider()
+            yield DropdownHeader(text=_("Variations"))
             parent = product.variation_parent
             yield DropdownItem(
                 text=_("Manage Variations"),
-                icon="fa fa-arrows-alt",
+                icon="fa fa-sitemap",
                 url=variation_url,
             )
             yield DropdownItem(
                 text=_("Parent: %s") % parent,
-                icon="fa fa-long-arrow-up",
+                icon="fa fa-eye",
                 url=get_model_url(parent),
             )
             for sib in product.get_variation_siblings():
                 yield DropdownItem(
                     text=_("Sibling: %s") % sib,
-                    icon="fa fa-long-arrow-right",
+                    icon="fa fa-eye",
                     url=get_model_url(sib),
                 )
         elif package_parent:
             yield DropdownDivider()
+            yield DropdownHeader(text=_("Variations"))
             yield DropdownItem(
                 text=_("Manage Package"),
-                icon="fa fa-archive",
+                icon="fa fa-cube",
                 url="#",  # TODO: Implement manage packages
             )
             for child in product.get_all_package_children():
                 yield DropdownItem(
                     text=_("Child: %s") % child,
-                    icon="fa fa-long-arrow-down",
+                    icon="fa fa-eye",
                     url=get_model_url(child),
                 )
 
         package_parents = list(product.get_all_package_parents())
         if package_parents:
             yield DropdownDivider()
+            yield DropdownHeader(text=_("Variations"))
             for parent in package_parents:
                 yield DropdownItem(
                     text=_("Package Parent: %s") % parent,
-                    icon="fa fa-long-arrow-up",
+                    icon="fa fa-eye",
                     url=get_model_url(parent),
                 )
 
         if not (variation_parent or variation_child or package_parent):
             yield DropdownDivider()
+            yield DropdownHeader(text=_("Variations"))
             yield DropdownItem(
                 text=_("Convert to Variation Parent"),
-                icon="fa fa-arrows-alt",
+                icon="fa fa-retweet",
                 url=variation_url,
             )
