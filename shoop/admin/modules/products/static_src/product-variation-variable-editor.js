3aef3508240bfe82e2a15a799b854e3f54658318
Juha Kujala <juha.kujala@shoop.io>
2015-08-26 06:11:54 +0000

    Change the layout for product variation editing
    Variation UI should now be more user friendly and the fields are now
    in one table instead in multiple columns. Also change the icons to
    describe the functionalities better. The tables showing variation children
    are also now unified to look quite same in every occasion.
    
    Refs SHOOP-1091

--- shoop/admin/modules/products/static_src/product-variation-variable-editor.js
+++ shoop/admin/modules/products/static_src/product-variation-variable-editor.js
@@ -32,7 +32,7 @@ window.VariationVariableEditor = (function(m, _) {
                 var changeXlateP = m.withAttr("value", _.partial(changeXlate, code));
                 var label = langLabelFormat.replace("$", name);
                 return m(cellSelector, [
-                    label,
+                    (label && label.length ? m("label", label) : null),
                     m("input.form-control", {
                         value: object[langKey][code] || "",
                         placeholder: label,
@@ -53,73 +53,87 @@ window.VariationVariableEditor = (function(m, _) {
         ];
     }
 
-    function valueTr(ctrl, value) {
+    function valueTr(ctrl, value, index) {
         var showIdentifierFields = ctrl.showIdentifierFields();
         return m("tr",
+            m("th", "Value " + (index + 1) + " Name"),
             getIdfrAndLanguagesCells("td", value, "texts", "", showIdentifierFields),
-            m("td", m("a", {href: "#", onclick: () => {
+            m("td", m("a.btn.text-danger.btn-xs", {href: "#", onclick: () => {
                 value.DELETE = true;
                 refreshField();
-            }}, m("i.fa.fa-minus"), " Delete value"))
+            }}, m("i.fa.fa-times-circle"), " Delete value"))
         );
     }
 
     function renderVariable(ctrl, variable) {
         var showIdentifierFields = ctrl.showIdentifierFields();
-        var deleteVariableButton = m("a", {href: "#", onclick: () => {
+        var deleteVariableButton = m("a.btn.btn-xs.text-danger", {href: "#", onclick: () => {
             if(variable.values.length === 0 || confirm("Are you sure?")) {
                 variable.DELETE = true;
                 refreshField();
             }
-        }}, m("i.fa.fa-minus"), " Delete variable");
-        var addValueButton = m("a", {href: "#", onclick: (event) => {
+        }}, m("i.fa.fa-times-circle"), " Delete Variable");
+        var addValueButton = m("a.btn.btn-xs.btn-text", {href: "#", onclick: (event) => {
             variable.values.push({pk: newPk(), identifier: "", texts: {}});
             event.preventDefault();
         }}, m("i.fa.fa-plus"), " Add new value");
-
-        var infoDiv = m("div", [
-            getIdfrAndLanguagesCells("div", variable, "names", "Variable Name ($)", showIdentifierFields),
-            m("br"),
-            deleteVariableButton
-        ]);
-        var valuesTable = m(
-            "table.table.table-condensed.table-bordered",
-            m("thead", m("tr", _.map(languages, ({name}) => m("th", "Value Text (" + name + ")")), m("td"))),
-            m("tbody", _.map(_.reject(variable.values, "DELETE"), _.partial(valueTr, ctrl)))
+        var bodyRows = [];
+        bodyRows.push(
+            m("tr", [m("th", "Variable Name")].concat(
+                getIdfrAndLanguagesCells("td", variable, "names", "", showIdentifierFields)
+            ).concat([m("td")]))
         );
-        var valuesDiv = m("div", [
-            valuesTable,
-            addValueButton
-        ]);
-        return m("div.variable", {key: variable.pk}, [
-            m("div.row", [
-                m("div.col-sm-4", infoDiv),
-                m("div.col-sm-8", valuesDiv)
+        bodyRows = bodyRows.concat(
+            _.map(_.reject(variable.values, "DELETE"), _.partial(valueTr, ctrl))
+        );
+        return m("div.product-variable", {key: variable.pk}, [
+            m("div.variable-heading.clearfix", [
+                m("h3.pull-left", "Variable"),
+                m("div.pull-right", deleteVariableButton)
+            ]),
+            m("div.variable-body", [
+                m("div.table-responsive", [
+                    m("table.table.table-bordered", [
+                        m("thead", [
+                            m("tr", [m("th")].concat(
+                                _.map(languages, ({name}) => m("th", name))
+                            ).concat([m("th")])),
+                        ]),
+                        m("tbody", bodyRows)
+                    ])
+                ]),
+                m("div.new-row", addValueButton)
             ])
         ]);
     }
 
     function view(ctrl) {
         var variablesDiv = m(
-            "div",
+            "div.product-variable-wrap",
             _.map(_.reject(variables, "DELETE"), _.partial(renderVariable, ctrl))
         );
-        var identifierFieldsCheckbox = m("label", [
-            m("input", {
-                type: "checkbox",
-                checked: !!ctrl.showIdentifierFields(),
-                onclick: m.withAttr("checked", ctrl.showIdentifierFields)
-            }),
-            " Show identifier fields (for advanced users)"
+        var identifierFieldsCheckbox = m("p", [
+            m("label.small", [
+                m("input", {
+                    type: "checkbox",
+                    checked: !!ctrl.showIdentifierFields(),
+                    onclick: m.withAttr("checked", ctrl.showIdentifierFields)
+                }),
+                " Show identifier fields (for advanced users)"
+            ])
         ]);
         if(!variables.length) {
-            variablesDiv = m("div.alert.alert-info", "There are no variables defined.");
+            variablesDiv = m("p.text-info", [
+                m("i.fa.fa-exclamation-circle"), " There are no variables defined.",
+                m("hr")
+            ]);
             identifierFieldsCheckbox = null;
         }
         return m("div", [
             identifierFieldsCheckbox,
+            m("hr"),
             variablesDiv,
-            m("a", {href: "#", onclick: (event) => {
+            m("a.btn.btn-lg.btn-text", {href: "#", onclick: (event) => {
                 variables.push({pk: newPk(), identifier: "", names: {}, values: []});
                 event.preventDefault();
             }}, m("i.fa.fa-plus"), " Add new variable")
