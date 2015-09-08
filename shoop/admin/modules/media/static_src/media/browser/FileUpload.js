477c20b785f784d9864430fc1d271e6c9c746ea6
Aarni Koskela <aarni.koskela@shoop.io>
2015-09-08 20:55:56 +0300

    Media browser: Add upload progress bar

--- shoop/admin/modules/media/static_src/media/browser/FileUpload.js
+++ shoop/admin/modules/media/static_src/media/browser/FileUpload.js
@@ -12,6 +12,41 @@ const _ = require("lodash");
 
 const queue = [];
 const queueCompleteCallbacks = [];
+var queueStatusDiv = null;
+
+function queueView() {
+    var className = "empty";
+    if(queue.length >= 0) {
+        className = (
+            _.all(queue, (file) => (file.status === "done" || file.status === "error")) ?
+                "done" : "busy"
+        );
+    }
+
+    return m("div.queue-view." + className, _.map(queue, (file) => {
+        return m("div.queue-file." + file.status, [
+            m("div.qf-name", file.name),
+            m("div.qf-progress", {style: "width: " + (file.progress || 0) + "%"})
+        ]);
+    }));
+}
+
+function updateQueueView() {
+    if(queueStatusDiv === null) {
+        if(queue.length === 0) {
+            return;  // Don't bother setting up the div if we're not doing anything actually
+        }
+        queueStatusDiv = document.createElement("div");
+        queueStatusDiv.id = "queue-status-ctr";
+        document.body.appendChild(queueStatusDiv);
+        // Yes, we're throwing away the ctrl instance; we don't need it
+        // and eslint would kvetch about it otherwise :)
+        m.mount(queueStatusDiv, {view: queueView, controller: _.noop});
+    }
+    m.redraw();  // XXX: It would be nice if we could redraw only one Mithril view...
+}
+
+const updateQueueViewSoon = _.debounce(updateQueueView, 50);
 
 function handleFileXhrComplete(xhr, file, error) {
     if (xhr.status >= 400) {
@@ -67,9 +102,11 @@ function beginUpload(file) {
             return;
         }
         handleFileXhrComplete(xhr, file, false);
+        updateQueueViewSoon();
     };
     xhr.onerror = function() {
         handleFileXhrComplete(xhr, file, true);
+        updateQueueViewSoon();
     };
     xhr.upload.onprogress = function(event) {
         if (event.lengthComputable) {
@@ -77,6 +114,7 @@ function beginUpload(file) {
         } else {
             file.progress = file.progress + (100 - file.progress) / 2;
         }
+        updateQueueViewSoon();
     };
     xhr.send(formData);
 }
@@ -106,6 +144,7 @@ export function processQueue() {
         return;  // Don't allow uploading multiple files simultaneously though...
     }
     const nextFile = _.detect(queue, (file) => (file.status === "new"));
+    updateQueueViewSoon();
     if (nextFile) {
         beginUpload(nextFile);
     } else {
