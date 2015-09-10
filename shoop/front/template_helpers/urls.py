faebb3526a98876d93ef333816ebf45c72b4fa41
Aarni Koskela <aarni.koskela@shoop.io>
2015-09-10 13:01:14 +0300

    Add `get_url`, sibling of `has_url`, to help with optimization.

--- shoop/front/template_helpers/urls.py
+++ shoop/front/template_helpers/urls.py
@@ -30,9 +30,38 @@ def model_url(context, model, absolute=False):
     return uri
 
 
-def has_url(url, *args, **kwargs):
+def get_url(url, *args, **kwargs):
+    """
+    Try to get the reversed URL for the given route name, args and kwargs.
+
+    If reverse resolution fails, returns None (instead of throwing an exception).
+
+    :param url: URL name.
+    :type url: str
+    :param args: URL args
+    :type args: Iterable[object]
+    :param kwargs: URL kwargs
+    :type kwargs: dict[str, object]
+    :return: Reversed URL or None
+    :rtype: str|None
+    """
     try:
-        reverse(url, args=args, kwargs=kwargs)
-        return True
+        return reverse(url, args=args, kwargs=kwargs)
     except NoReverseMatch:
-        return False
+        return None
+
+
+def has_url(url, *args, **kwargs):
+    """
+    Try to get the reversed URL for the given route name, args and kwargs and return a success flag.
+
+    :param url: URL name.
+    :type url: str
+    :param args: URL args
+    :type args: Iterable[object]
+    :param kwargs: URL kwargs
+    :type kwargs: dict[str, object]
+    :return: Success flag
+    :rtype: bool
+    """
+    return bool(get_url(url, *args, **kwargs))
