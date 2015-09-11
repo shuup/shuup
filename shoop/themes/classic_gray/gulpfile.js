aa6e3afe9d86edd6c77639e11f50fb11c39eeca7
Panu Tulimäki <panu.tulimaki@andersinnovations.com>
2015-09-11 10:21:14 +0000

    Classic gray theme: Add missing bootstrap-select to bower.json
    Refs SHOOP-1178

--- shoop/themes/classic_gray/gulpfile.js
+++ shoop/themes/classic_gray/gulpfile.js
@@ -29,7 +29,7 @@ gulp.task("js", function() {
     return gulp.src([
         "bower_components/jquery/dist/jquery.js",
         "bower_components/bootstrap/dist/js/bootstrap.js",
-        "bower_components/bootstrap-select/js/bootstrap-select.js",
+        "bower_components/bootstrap-select/dist/js/bootstrap-select.js",
         "static_src/js/custom.js"
     ])
         .pipe(plumber({}))
