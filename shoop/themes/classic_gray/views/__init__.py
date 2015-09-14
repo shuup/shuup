b5aa829079434354f772ebfce0db0d4db57941a1
Panu Tulimäki <panu.tulimaki@anders.fi>
2015-09-14 18:35:00 +0300

    Classic gray theme: Use views module for extra views

---
+++ shoop/themes/classic_gray/views/__init__.py
@@ -0,0 +1,6 @@
+# This file is part of Shoop.
+#
+# Copyright (c) 2012-2015, Shoop Ltd. All rights reserved.
+#
+# This source code is licensed under the AGPLv3 license found in the
+# LICENSE file in the root directory of this source tree.
