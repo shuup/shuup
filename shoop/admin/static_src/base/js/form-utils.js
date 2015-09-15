d3ae48fecccc85b1e22a7a3913ecf196e4cf14d1
Aarni Koskela <aarni.koskela@shoop.io>
2015-09-15 17:49:48 +0300

    Admin: merge form-related JavaScript into one file

--- shoop/admin/static_src/base/js/form-utils.js
+++ shoop/admin/static_src/base/js/form-utils.js
@@ -6,6 +6,7 @@
  * This source code is licensed under the AGPLv3 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 window.setNextActionAndSubmit = function(formId, nextAction) {
     const $form = $("#" + formId);
     if(!$form.length) {
@@ -22,3 +23,21 @@ window.setNextActionAndSubmit = function(formId, nextAction) {
     $nextAction.val(nextAction);
     $form.submit();
 };
+
+$(function() {
+    const isMobile = !!(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent));
+    $(".multiselect").selectpicker({
+        mobile: isMobile,
+        style: "btn btn-select",
+        title: "",
+        selectedTextFormat: "count > 3",
+        countSelectedText: "{0}/{1} selected"
+    });
+    const $dropdowns = $(".dropdown");
+    // Add slideUp/slideDown animations to all bootstrap dropdowns
+    $dropdowns.on("show.bs.dropdown", function() {
+        $(this).find(".dropdown-menu").first().stop(true, true).slideDown(200, "easeInSine");
+    }).on("hide.bs.dropdown", function() {
+        $(this).find(".dropdown-menu").first().stop(true, true).slideUp(300, "easeOutSine");
+    });
+}());
