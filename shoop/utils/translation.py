bab0a81afb207eec22ff237ede8eebc55f8411f0
Aarni Koskela <aarni.koskela@shoop.io>
2015-09-16 13:40:26 +0300

    Fix `cache_translations` to work with new Parler
    `cache_translations` had quietly broken when Parler was upgraded past
    commit 601968c956c90e8bbc83d99cd87e18194790f2fb (Parler 1.2+), causing
    caching to be done but Parler never knowing how to use the cached
    translation data.

--- shoop/utils/translation.py
+++ shoop/utils/translation.py
@@ -7,10 +7,11 @@
 # LICENSE file in the root directory of this source tree.
 
 
-def cache_translations(objects, languages=None):
+def cache_translations(objects, languages=None, meta=None):
     """
     Cache translation objects in given languages to the objects in one fell swoop.
     This will iterate a queryset, if one is passed!
+
     :param objects: List or queryset of Translatable models
     :param languages: Iterable of languages to fetch. In addition, all "_current_language"s will be fetched
     :return: objects
@@ -18,11 +19,15 @@ def cache_translations(objects, languages=None):
     if not objects:
         return objects
     languages = set(languages or ())
-    xlate_model = objects[0]._parler_meta.root_model
+    if meta is None:
+        meta = objects[0]._parler_meta.root  # work on base model by default
+    xlate_model = meta.model
+
     object_map = dict((object.pk, object) for object in objects)
     languages.update(set(object._current_language for object in objects))
-    for translation in xlate_model.objects.filter(master_id__in=object_map.keys(), language_code__in=languages):
+    master_ids = object_map.keys()
+    for translation in xlate_model.objects.filter(master_id__in=master_ids, language_code__in=languages):
         master = object_map[translation.master_id]
-        master._translations_cache[translation.language_code] = translation
+        master._translations_cache[xlate_model][translation.language_code] = translation
         setattr(translation, translation.__class__.master.cache_name, master)
     return objects
