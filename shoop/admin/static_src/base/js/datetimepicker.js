7291e77271fda45875098a5805ebde19842a0f15
Juha Kujala <juha.kujala@shoop.io>
2015-10-08 09:06:53 +0000

    Admin: Add script for datepicker
    Refs SHOOP-1624

--- shoop/admin/static_src/base/js/datetimepicker.js
+++ shoop/admin/static_src/base/js/datetimepicker.js
@@ -1,9 +1,17 @@
 $(function() {
-    $(".datetime").datetimepicker({
+    $(".form-control.datetime").datetimepicker({
         format: 'yyyy-mm-dd hh:ii',
         autoclose: true,
         todayBtn: true,
         todayHighlight: true,
         fontAwesome: true
+    });
+    $(".form-control.date").datetimepicker({
+        format: 'yyyy-mm-dd',
+        autoclose: true,
+        todayBtn: true,
+        todayHighlight: true,
+        fontAwesome: true,
+        minView: 2
     });
 });
