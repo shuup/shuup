505eecf3a4c8c4e674c98074490168c325f94b17
Janne Tammi <janne.tammi@shoop.io>
2015-10-14 10:47:35 +0300

    XTheme: simplify column width configuration in editor
    Use only one select to set both `sm` and `md` values
    
    Refs SHOOP-1688

--- shoop_tests/xtheme/test_editor_forms.py
+++ shoop_tests/xtheme/test_editor_forms.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from shoop.xtheme.layout import LayoutCell
-from shoop.xtheme.views.forms import LayoutCellFormGroup
+from shoop.xtheme.views.forms import LayoutCellFormGroup, LayoutCellGeneralInfoForm
 from shoop_tests.xtheme.utils import plugin_override
 
 
@@ -13,28 +13,30 @@ def test_pluginless_lcfg():
 
 
 def test_formless_plugin_in_lcfg():
+    two_thirds = int(LayoutCellGeneralInfoForm.CELL_FULL_WIDTH * 2 / 3)
     with plugin_override():
         cell = LayoutCell("inject")
         assert cell.instantiate_plugin()
-        lcfg = LayoutCellFormGroup(data={"general-size_md": "8"}, layout_cell=cell)
+        lcfg = LayoutCellFormGroup(data={"general-cell_width": "%d" % two_thirds}, layout_cell=cell)
         assert "plugin" not in lcfg.forms
         assert lcfg.is_valid()
         lcfg.save()
-        assert cell.sizes["md"] == 8  # Something got saved even if the plugin doesn't need config
+        assert cell.sizes["md"] == two_thirds  # Something got saved even if the plugin doesn't need config
 
 
 def test_lcfg():
+    two_thirds = int(LayoutCellGeneralInfoForm.CELL_FULL_WIDTH * 2 / 3)
     with plugin_override():
-        cell = LayoutCell("text", sizes={"md": 8})
+        cell = LayoutCell("text", sizes={"md": two_thirds, "sm": two_thirds})
         lcfg = LayoutCellFormGroup(layout_cell=cell)
         assert "general" in lcfg.forms
         assert "plugin" in lcfg.forms
         assert not lcfg.is_valid()  # Oh, we must've forgotten the text...
         lcfg = LayoutCellFormGroup(data={
-            "general-size_md": "8",
+            "general-cell_width": "%d" % two_thirds,
             "plugin-text": "Hello, world!"
         }, layout_cell=cell)
         assert lcfg.is_valid()  # Let's see now!
         lcfg.save()
-        assert cell.sizes["md"] == 8
+        assert cell.sizes["md"] == two_thirds
         assert cell.config["text"] == "Hello, world!"
