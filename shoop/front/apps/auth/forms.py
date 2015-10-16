5801de8b2d6a385c79f4faa7ea60804701e391a4
Panu Tulimäki <panu.tulimaki@anders.fi>
2015-10-16 13:02:04 +0300

    Auth: prevent attackers to know which emails is in use
    Give always same error message when login fails. Also prevent timing attacks in cases when username or email is not used.
    
    Refs SHOOP-1670 / SHOOP-1703

--- shoop/front/apps/auth/forms.py
+++ shoop/front/apps/auth/forms.py
@@ -9,7 +9,7 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.tokens import default_token_generator
-from django.core.exceptions import MultipleObjectsReturned
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.core.mail import send_mail
 from django.db.models import Q
 from django.template import loader
@@ -20,6 +20,13 @@ from django.utils.translation import ugettext as _
 
 class EmailAuthenticationForm(AuthenticationForm):
 
+    error_messages = {
+        'invalid_login': _("Please enter a correct %(username)s and password. "
+                           "Note that both fields may be case-sensitive. "
+                           "In case of multiple accounts with same email only username can be used to login."),
+        'inactive': _("This account is inactive."),
+    }
+
     def __init__(self, *args, **kwargs):
         super(EmailAuthenticationForm, self).__init__(*args, **kwargs)
         self.fields['username'].label = _("Username or email address")
@@ -27,14 +34,21 @@ class EmailAuthenticationForm(AuthenticationForm):
     def clean_username(self):
         username = self.data['username']
         user_model = get_user_model()
-        if '@' in username:
-            try:
-                username = user_model.objects.get(email=username).username
-            except MultipleObjectsReturned:
-                raise forms.ValidationError(
-                    _("Multiple users have this email, please use username to login."),
-                    code='invalid_login_multiple_users'
-                )
+
+        # Note: Always search by username AND by email prevent timing attacks
+        try:
+            user_by_name = user_model._default_manager.get_by_natural_key(username)
+        except ObjectDoesNotExist:
+            user_by_name = None
+
+        try:
+            user_by_email = user_model._default_manager.get(email=username)
+        except (ObjectDoesNotExist, MultipleObjectsReturned):
+            user_by_email = None
+
+        if not user_by_name and user_by_email:
+            return getattr(user_by_email, user_model.USERNAME_FIELD)
+
         return username
 
 
