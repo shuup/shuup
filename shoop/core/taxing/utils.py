34e99f304d77629e7896d719e614033ebab0bcb2
Tuomas Suutari <tuomas.suutari@shoop.io>
2015-10-22 10:47:38 +0300

    Implement calculation of compounded taxes
    Add function `calculate_compounded_added_taxes` to
    `shoop.core.taxing.utils` which calculates the compounded and added
    taxes from specified tax groups.
    
    Also add tests for this.  This also tests the previously too lightly
    tested `stacked_value_added_taxes` function.
    
    Refs SHOOP-1636/SHOOP-1622

--- shoop/core/taxing/utils.py
+++ shoop/core/taxing/utils.py
@@ -51,3 +51,52 @@ def stacked_value_added_taxes(price, taxes):
         taxful = TaxfulPrice(taxless.amount + total_tax_amount)
 
     return TaxedPrice(taxful, taxless, line_taxes)
+
+
+def calculate_compounded_added_taxes(price, tax_groups):
+    """
+    Calculate compounded and added taxes from given groups of taxes.
+
+    The `tax_groups` argument should be a list of tax groups, where each
+    tax group is a list of `Tax` objects.  Taxes in each tax group will
+    be added together and finally each added tax group will be
+    compounded over each other.
+
+    :param price: Taxful or taxless price to calculate taxes for
+    :type price: shoop.core.pricing.Price
+    :param tax_groups: List of tax groups, each being a list of taxes
+    :type tax_groups: list[list[shoop.core.models.Tax]]
+    :return: TaxedPrice with the calculated taxes.
+    :rtype: TaxedPrice
+    """
+    if price.includes_tax:
+        return _calc_compounded_added_taxes_from_taxful(price, tax_groups)
+    else:
+        return _calc_compounded_added_taxes_from_taxless(price, tax_groups)
+
+
+def _calc_compounded_added_taxes_from_taxful(amount, tax_groups):
+    base_price = TaxfulPrice(amount)
+    reversed_line_taxes = []
+    for taxes in reversed(tax_groups):
+        taxed_price = stacked_value_added_taxes(base_price, taxes)
+        base_price = TaxfulPrice(taxed_price.taxless)
+        reversed_line_taxes.extend(reversed(taxed_price.taxes))
+    line_taxes = list(reversed(reversed_line_taxes))
+    return TaxedPrice(
+        taxful=TaxfulPrice(amount),
+        taxless=TaxlessPrice(base_price),
+        taxes=line_taxes)
+
+
+def _calc_compounded_added_taxes_from_taxless(amount, tax_groups):
+    base_price = TaxlessPrice(amount)
+    line_taxes = []
+    for taxes in tax_groups:
+        taxed_price = stacked_value_added_taxes(base_price, taxes)
+        base_price = TaxlessPrice(taxed_price.taxful)
+        line_taxes.extend(taxed_price.taxes)
+    return TaxedPrice(
+        taxful=TaxfulPrice(base_price),
+        taxless=TaxlessPrice(amount),
+        taxes=line_taxes)
