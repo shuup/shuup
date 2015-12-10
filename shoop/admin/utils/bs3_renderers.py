27b842a65b2cf3a40263a3b2c31309de642cc069
Tuomas Suutari <tuomas.suutari@shoop.io>
2015-12-10 09:05:54 +0200

    bs3_renderers: Support for newer django-boostrap3
    Newer version of django-boostrap3 adds optional widget argument to
    `add_class_attrs` and `add_help_attrs` methods.  Pass it and any other
    arguments to super via *args and **kwargs and use the passed-in widget
    if there is one.
    
    Refs SHOOP-1942/SHOOP-1954

--- shoop/admin/utils/bs3_renderers.py
+++ shoop/admin/utils/bs3_renderers.py
@@ -41,17 +41,19 @@ class AdminFieldRenderer(FieldRenderer):
             return None
         return super(AdminFieldRenderer, self).get_label()
 
-    def add_class_attrs(self):
-        super(AdminFieldRenderer, self).add_class_attrs()
+    def add_class_attrs(self, *args, **kwargs):
+        widget = kwargs.get('widget') or self.widget
+        super(AdminFieldRenderer, self).add_class_attrs(*args, **kwargs)
         if self.widget_class:
-            classes = self.widget.attrs.get('class', '')
+            classes = widget.attrs.get('class', '')
             classes = add_css_class(classes, self.widget_class)
-            self.widget.attrs['class'] = classes
+            widget.attrs['class'] = classes
 
-    def add_help_attrs(self):
-        super(AdminFieldRenderer, self).add_help_attrs()
-        if not self.widget.attrs.get("title"):
-            self.widget.attrs.pop("title", None)  # Remove the empty attribute
+    def add_help_attrs(self, *args, **kwargs):
+        widget = kwargs.get('widget') or self.widget
+        super(AdminFieldRenderer, self).add_help_attrs(*args, **kwargs)
+        if not widget.attrs.get("title"):
+            widget.attrs.pop("title", None)  # Remove the empty attribute
 
         if not self.show_help_block:  # Remove field help to avoid rendering `help-block`
             self.field_help = ''
