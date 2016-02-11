954b40d67d063459537e7c015caf8aced93cc7b2
Panu Tulimäki <panu.tulimaki@anders.fi>
2016-02-11 02:36:10 +0200

    Campaigns: add clean for campaign start and end
    End date can't be before start date.
    
    Refs SHOOP-2107

--- shoop/campaigns/forms.py
+++ shoop/campaigns/forms.py
@@ -86,14 +86,18 @@ class CampaignFormMixin(object):
                     self.fields[obj.identifier] = field.formfield(**kwargs)
 
     def clean(self):
-        """ Ensure atleast one rule is set """
+        """ Ensure at least one rule is set """
         data = super(CampaignFormMixin, self).clean()
-
         if data.get("discount_percentage") and data.get("discount_amount_value"):
             raise ValidationError(_("You should only define either discount percentage or amount."))
 
         if not any([data.get("discount_percentage"), data.get("discount_amount_value")]):
             raise ValidationError(_("You must define discount percentage or amount."))
+
+        start_datetime = data.get("start_datetime")
+        end_datetime = data.get("end_datetime")
+        if start_datetime and end_datetime and end_datetime < start_datetime:
+            raise ValidationError(_("Campaign end date can't be before start date."), code="end_is_before_start")
 
         return data
 
@@ -108,11 +112,6 @@ class CampaignFormMixin(object):
         if amount and amount < 0:
             raise ValidationError(_("Discount amount cannot be negative."))
         return amount
-
-    def clean_end_datetime(self):
-        end_datetime = self.cleaned_data.get("end_datetime")
-        # TODO: allow only end dates that are after start date
-        return end_datetime
 
     def save(self, commit=True):
         instance = super(CampaignFormMixin, self).save(commit)
