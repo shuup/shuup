94ae30f253019b5543f7d16347b630888a76482c
Shawn Her Many Horses <shawn.hermanyhorses@anders.fi>
2016-02-29 15:55:18 -0800

    Front: Clean up commented code
    Remove commented code from template helpers

--- shoop/front/template_helpers/general.py
+++ shoop/front/template_helpers/general.py
@@ -24,22 +24,6 @@ def _get_list_products(request):
     )
 
 
-# @contextfunction
-# def get_recommended_products(context):
-#     func = cached_load("SHOP_GLOBAL_RECOMMENDED_PRODUCTS_SPEC", lambda request: [])
-#     request = context["request"]
-#     products = func(request)
-#     if products and any(not p for p in products):
-#         fillins = list(get_best_selling_products(context)) + list(get_newest_products(context))
-#         if settings.SHOP_ENABLE_VARIATIONS:
-#             fillins = list(set((p.variation_parent or p) for p in fillins))
-#         random.shuffle(fillins)
-#         products = [(p if p else (fillins.pop(0) if fillins else None)) for p in products]
-#     products = [p for p in products if p and p.is_visible()]
-#     cache_product_things(request, products)
-#     return products
-
-
 @contextfunction
 def get_best_selling_products(context, n_products=12, cutoff_days=30, no_variation_children=False):
     request = context["request"]
