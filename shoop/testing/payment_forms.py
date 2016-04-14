244ec6447a144bfff076b1de61de591961a131b5
Panu Tulimäki <panu.tulimaki@anders.fi>
2016-04-13 20:35:47 -0700

    Admin: Add generic ShoopAdminForm
    * Inherited from ``MultiLanguageModelForm``
    * Updates ``MediaChoiceWidget`` for fields having ``filer.fields.file.AdminFileWidget``
    
    Refs SHOOP-2071

--- shoop/testing/payment_forms.py
+++ shoop/testing/payment_forms.py
@@ -5,18 +5,18 @@
 # This source code is licensed under the AGPLv3 license found in the
 # LICENSE file in the root directory of this source tree.
 
-from shoop.utils.multilanguage_model_form import MultiLanguageModelForm
+from shoop.admin.forms import ShoopAdminForm
 
 from .models import PaymentWithCheckoutPhase, PseudoPaymentProcessor
 
 
-class PseudoPaymentProcessorForm(MultiLanguageModelForm):
+class PseudoPaymentProcessorForm(ShoopAdminForm):
     class Meta:
         model = PseudoPaymentProcessor
         exclude = ["identifier"]
 
 
-class PaymentWithCheckoutPhaseForm(MultiLanguageModelForm):
+class PaymentWithCheckoutPhaseForm(ShoopAdminForm):
     class Meta:
         model = PaymentWithCheckoutPhase
         exclude = ["identifier"]
