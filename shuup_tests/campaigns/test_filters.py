672b011a28229d0d95e765f375bb794db52b022f
Janne Tammi <janne.tammi@shoop.io>
2016-08-02 11:27:35 +0300

    Fix a bug with ProductFilter

--- shuup_tests/campaigns/test_filters.py
+++ shuup_tests/campaigns/test_filters.py
@@ -5,14 +5,19 @@
 # This source code is licensed under the AGPLv3 license found in the
 # LICENSE file in the root directory of this source tree.
 import pytest
+from decimal import Decimal
 from django.utils.encoding import force_text
 
+from shuup.campaigns.models import CatalogCampaign
 from shuup.campaigns.models.catalog_filters import (
     CatalogFilter, CategoryFilter, ProductFilter, ProductTypeFilter
 )
+from shuup.campaigns.models.product_effects import ProductDiscountPercentage
 from shuup.core.models import Category, ShopProduct
-from shuup.testing.factories import create_product, get_default_category
+from shuup.front.basket import get_basket
+from shuup.testing.factories import create_product, get_default_category, get_default_supplier
 from shuup_tests.campaigns import initialize_test
+from shuup_tests.utils import printable_gibberish
 
 
 @pytest.mark.django_db
@@ -85,3 +90,33 @@ def test_product_type_filter(rf):
     assert product_type_filter.filter_queryset(ShopProduct.objects.all()).exists()  # filter matches
 
     assert product_type_filter.name.lower() in force_text(product_type_filter.description)
+
+
+@pytest.mark.django_db
+def test_productfilter_works(rf):
+    request, shop, group = initialize_test(rf, False)
+    price = shop.create_price
+    product_price = "100"
+    discount_percentage = "0.30"
+
+    supplier = get_default_supplier()
+    product = create_product(printable_gibberish(), shop=shop, supplier=supplier, default_price=product_price)
+    shop_product = product.get_shop_instance(shop)
+
+    # create catalog campaign
+    catalog_filter = ProductFilter.objects.create()
+    catalog_filter.products.add(product)
+
+    assert catalog_filter.matches(shop_product)
+
+    catalog_campaign = CatalogCampaign.objects.create(shop=shop, active=True, name="test")
+    catalog_campaign.filters.add(catalog_filter)
+    cdp = ProductDiscountPercentage.objects.create(campaign=catalog_campaign, discount_percentage=discount_percentage)
+
+    # add product to basket
+    basket = get_basket(request)
+    basket.add_product(supplier=supplier, shop=shop, product=product, quantity=1)
+    basket.save()
+
+    expected_total = price(product_price) - (Decimal(discount_percentage) * price(product_price))
+    assert basket.total_price == expected_total
