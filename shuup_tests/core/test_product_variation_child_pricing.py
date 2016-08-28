019bd44ad3c260010cc34d4249ece6da58405e68
Janne Tammi <janne@shuup.com>
2016-08-28 13:09:53 -0700

    Add missing licence headers to tests
    No ref

--- shuup_tests/core/test_product_variation_child_pricing.py
+++ shuup_tests/core/test_product_variation_child_pricing.py
@@ -1,3 +1,9 @@
+# This file is part of Shuup.
+#
+# Copyright (c) 2012-2016, Shoop Ltd. All rights reserved.
+#
+# This source code is licensed under the AGPLv3 license found in the
+# LICENSE file in the root directory of this source tree.
 import pytest
 
 from shuup.core.pricing import TaxfulPrice, TaxlessPrice
