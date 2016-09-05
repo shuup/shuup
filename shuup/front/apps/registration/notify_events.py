238d56b842b1b02f8c2d4c21aee31a2558e2a81e
Christian Hess <christianhess.rlz@gmail.com>
2016-09-05 09:13:46 -0300

    Translate notification names and variables

--- shuup/front/apps/registration/notify_events.py
+++ shuup/front/apps/registration/notify_events.py
@@ -6,6 +6,7 @@
 # This source code is licensed under the AGPLv3 license found in the
 # LICENSE file in the root directory of this source tree.
 from django.dispatch import receiver
+from django.utils.translation import ugettext_lazy as _
 from registration.signals import user_registered
 
 from shuup.notify import Event, Variable
@@ -14,8 +15,9 @@ from shuup.notify.typology import Email
 
 class RegistrationReceived(Event):
     identifier = "registration_received"
+    name = _("Registration Received")
 
-    customer_email = Variable("Customer Email", type=Email)
+    customer_email = Variable(_("Customer Email"), type=Email)
 
 
 @receiver(user_registered)
