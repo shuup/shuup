213cade8f31e07d2d47597c622c87b84864ac248
Panu Tulimäki <panu@shuup.com>
2016-10-05 22:36:48 -0700

    Front: Add base and admin for dynamic sorts and filters
    Add option to specify product lists sorts and filters through
    admin.
    
    Product list form used in category and will be replaced with
    to use `shuup.front.utils.sorts_and_filters.ProductListForm` for
    sorts and filters. Product list form can be extended with
    ``front_extend_product_list_form`` provider class that implements
    the `shuup.front.utils.sorts_and_filters.ProductListFormModifier`
    interface.
    
    Refs SH-284 / SH-343

---
+++ shuup/front/admin_module/sorts_and_filters/form_parts.py
@@ -0,0 +1,63 @@
+# This file is part of Shuup.
+#
+# Copyright (c) 2012-2016, Shoop Commerce Ltd. All rights reserved.
+#
+# This source code is licensed under the AGPLv3 license found in the
+# LICENSE file in the root directory of this source tree.
+from __future__ import unicode_literals
+
+from django import forms
+
+from shuup.admin.form_part import FormPart, TemplatedFormDef
+from shuup.apps.provides import get_provide_objects
+from shuup.front.utils.sorts_and_filters import (
+    FORM_MODIFIER_PROVIDER_KEY, get_configuration, set_configuration
+)
+
+
+class ConfigurationForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(ConfigurationForm, self).__init__(*args, **kwargs)
+        for extend_class in get_provide_objects(FORM_MODIFIER_PROVIDER_KEY) or []:
+            for field_key, field in extend_class().get_admin_fields() or []:
+                self.fields[field_key] = field
+
+
+class ConfigurationShopFormPart(FormPart):
+    priority = 7
+    name = "product_list_facets"
+    form = ConfigurationForm
+
+    def get_form_defs(self):
+        if not self.object.pk:
+            return
+        yield TemplatedFormDef(
+            name=self.name,
+            form_class=self.form,
+            template_name="shuup/front/admin/sorts_and_filters.jinja",
+            required=False,
+            kwargs={"initial": get_configuration(shop=self.object)})
+
+    def form_valid(self, form):
+        if self.name in form.forms:
+            set_configuration(shop=self.object, data=form[self.name].cleaned_data)
+
+
+class ConfigurationCategoryFormPart(FormPart):
+    priority = 7
+    name = "product_list_facets"
+    form = ConfigurationForm
+
+    def get_form_defs(self):
+        if not self.object.pk:
+            return
+        yield TemplatedFormDef(
+            name=self.name,
+            form_class=self.form,
+            template_name="shuup/front/admin/sorts_and_filters.jinja",
+            required=False,
+            kwargs={"initial": get_configuration(category=self.object)})
+
+    def form_valid(self, form):
+        if self.name in form.forms:
+            set_configuration(category=self.object, data=form[self.name].cleaned_data)
---
+++ shuup_tests/functional/test_sorts_and_filters_admin.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+# This file is part of Shuup.
+#
+# Copyright (c) 2012-2016, Shoop Commerce Ltd. All rights reserved.
+#
+# This source code is licensed under the AGPLv3 license found in the
+# LICENSE file in the root directory of this source tree.
+import pytest
+
+from django.conf import settings
+from django.utils.translation import activate
+
+from shuup import configuration
+from shuup.core import cache
+from shuup.admin.modules.categories.views import CategoryEditView
+from shuup.admin.modules.shops.views import ShopEditView
+from shuup.apps.provides import override_provides
+from shuup.front.utils.sorts_and_filters import get_configuration
+from shuup.testing.factories import get_default_category, get_default_shop
+from shuup.testing.utils import apply_request_middleware
+
+
+DEFAULT_FORM_MODIFIERS = [
+    "shuup.front.forms.product_list_modifiers.SortProductListByName",
+    "shuup.front.forms.product_list_modifiers.SortProductListByPrice",
+    "shuup.front.forms.product_list_modifiers.ManufacturerProductListFilter",
+]
+
+
+@pytest.mark.django_db
+def test_sorts_and_filter_in_shop_edit(rf, admin_user):
+    cache.clear()
+    activate("en")
+    with override_provides("front_extend_product_list_form", DEFAULT_FORM_MODIFIERS):
+        shop = get_default_shop()
+        view = ShopEditView.as_view()
+        assert get_configuration(shop=shop) == settings.SHUUP_FRONT_DEFAULT_SORT_CONFIGURATION
+        data = {
+            "base-name__en": shop.name,
+            "base-public_name__en": shop.public_name,
+            "base-status": shop.status.value,
+            "base-currency": shop.currency,
+            "base-prices_include_tax": shop.prices_include_tax,
+            "base-languages": "en",
+            "product_list_facets-sort_products_by_name": True,
+            "product_list_facets-sort_products_by_name_ordering": 11,
+            "product_list_facets-sort_products_by_price": False,
+            "product_list_facets-sort_products_by_price_ordering": 32,
+            "product_list_facets-filter_products_by_manufacturer": False,
+            "product_list_facets-filter_products_by_manufacturer_ordering": 1
+        }
+        request = apply_request_middleware(rf.post("/", data=data), user=admin_user)
+        response = view(request, pk=shop.pk)
+        if hasattr(response, "render"):
+            response.render()
+        assert response.status_code in [200, 302]
+        expected_configurations = {
+            "sort_products_by_name": True,
+            "sort_products_by_name_ordering": 11,
+            "sort_products_by_price": False,
+            "sort_products_by_price_ordering": 32,
+            "filter_products_by_manufacturer": False,
+            "filter_products_by_manufacturer_ordering": 1
+        }
+        assert get_configuration(shop=shop) == expected_configurations
+
+
+@pytest.mark.django_db
+def test_sorts_and_filter_in_category_edit(rf, admin_user):
+    get_default_shop()
+    cache.clear()
+    activate("en")
+    with override_provides("front_extend_product_list_form", DEFAULT_FORM_MODIFIERS):
+        category = get_default_category()
+        view = CategoryEditView.as_view()
+        assert get_configuration(category=category) == settings.SHUUP_FRONT_DEFAULT_SORT_CONFIGURATION
+        data = {
+            "base-name__en": category.name,
+            "base-status": category.status.value,
+            "base-visibility": category.visibility.value,
+            "base-ordering": category.ordering,
+            "product_list_facets-sort_products_by_name": True,
+            "product_list_facets-sort_products_by_name_ordering": 6,
+            "product_list_facets-sort_products_by_price": False,
+            "product_list_facets-sort_products_by_price_ordering": 32,
+            "product_list_facets-filter_products_by_manufacturer": True,
+            "product_list_facets-filter_products_by_manufacturer_ordering": 1
+        }
+        request = apply_request_middleware(rf.post("/", data=data), user=admin_user)
+        response = view(request, pk=category.pk)
+        if hasattr(response, "render"):
+            response.render()
+        assert response.status_code in [200, 302]
+        expected_configurations = {
+            "sort_products_by_name": True,
+            "sort_products_by_name_ordering": 6,
+            "sort_products_by_price": False,
+            "sort_products_by_price_ordering": 32,
+            "filter_products_by_manufacturer": True,
+            "filter_products_by_manufacturer_ordering": 1
+        }
+        assert get_configuration(category=category) == expected_configurations
