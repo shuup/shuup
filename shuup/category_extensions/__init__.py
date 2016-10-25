9c8f945418ca6703477f2c693f3df9361c1d4995
Janne Tammi <janne@shuup.com>
2016-10-25 15:16:18 -0700

    Add Category Extensions
    Currently adds a way to automatically populate products into categories.
    
    Refs PP-5

---
+++ shuup/category_extensions/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# This file is part of Shuup.
+#
+# Copyright (c) 2012-2016, Shoop Commerce Ltd. All rights reserved.
+#
+# This source code is licensed under the AGPLv3 license found in the
+# LICENSE file in the root directory of this source tree.
+default_app_config = "shuup.category_extensions.apps.CategoryExtensionsAppConfig"
