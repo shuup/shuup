da786ec65fead17f1b79e753cfb53131744364ed
Gurch Rai <gurch@shuup.com>
2016-11-04 10:52:41 -0700

    Admin: Show default columns that don't belong to table
    Aggregate columns were previously dropped from column lists. Ensure
    they are present and selectable.
    
    Refs SH-408

--- shuup/admin/modules/settings/views/__init__.py
+++ shuup/admin/modules/settings/views/__init__.py
@@ -6,7 +6,7 @@
 # LICENSE file in the root directory of this source tree.
 import six
 from django.contrib import messages
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import resolve, reverse
 from django.http import HttpResponseRedirect
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic import FormView
@@ -23,9 +23,11 @@ class ListSettingsView(FormView):
 
     def dispatch(self, request, *args, **kwargs):
         module_str = "%s:%s" % (request.GET.get("module"), request.GET.get("model"))
-        self.return_url = request.GET.get("return_url")
+        self.return_url = reverse("shuup_admin:%s.list" % request.GET.get("return_url"))
+        match = resolve(self.return_url)
+        default_columns = load("%s:%s" % (match.func.__module__, match.func.__name__)).default_columns
         self.model = load(module_str)
-        self.settings = ViewSettings(self.model, [])
+        self.settings = ViewSettings(self.model, default_columns)
         return super(ListSettingsView, self).dispatch(request, *args, **kwargs)
 
     def get_form(self, form_class=None):
@@ -45,7 +47,7 @@ class ListSettingsView(FormView):
         for col, val in six.iteritems(form.cleaned_data):
             self.settings.set_config(col, val, use_key=True)
         messages.success(self.request, _("Settings saved"), fail_silently=True)
-        return HttpResponseRedirect(reverse("shuup_admin:%s.list" % self.return_url))
+        return HttpResponseRedirect(self.return_url)
 
     def get_context_data(self, **kwargs):
         context = super(ListSettingsView, self).get_context_data(**kwargs)
