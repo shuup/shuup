b74bb47fd879c19876534a27ef7cdb615a892eb7
Joa Riski <joa.riski@anders.fi>
2016-11-21 15:28:39 +0200

    Make test_analog compatible with Django 1.8
    The default related object manager and it's fieldnames change in Django 1.9
    As the test_analog test uses these fields, some minor changes are required to keep it compatible
    
    Fix the test by doing some special handling depending on the version
    
    No refs

--- shuup_tests/utils/test_analog.py
+++ shuup_tests/utils/test_analog.py
@@ -5,7 +5,7 @@
 #
 # This source code is licensed under the AGPLv3 license found in the
 # LICENSE file in the root directory of this source tree.
-from django.db import models
+from django import VERSION
 
 from shuup.testing.models import PseudoPaymentProcessor
 from shuup.utils.analog import BaseLogEntry, define_log_model
@@ -14,8 +14,18 @@ from shuup.utils.analog import BaseLogEntry, define_log_model
 def test_analog():
     PseudoPaymentProcessorLogEntry = define_log_model(PseudoPaymentProcessor)
     assert PseudoPaymentProcessorLogEntry.__module__ == PseudoPaymentProcessor.__module__
-    assert PseudoPaymentProcessorLogEntry._meta.get_field("target").rel.to is PseudoPaymentProcessor
-    assert PseudoPaymentProcessor.log_entries.rel.model is PseudoPaymentProcessor
-    assert PseudoPaymentProcessor.log_entries.rel.related_model is PseudoPaymentProcessorLogEntry
+
+    related_field_name = "related"
+    # Behavior changs in Django 1.9
+    if VERSION >= (1, 9):
+        related_field_name = "rel"
+
+    relation_manager = getattr(PseudoPaymentProcessorLogEntry._meta.get_field("target"), related_field_name)
+    assert relation_manager.to is PseudoPaymentProcessor
+
+    relation_manager = getattr(PseudoPaymentProcessor.log_entries, related_field_name)
+    assert relation_manager.model is PseudoPaymentProcessor
+    assert relation_manager.related_model is PseudoPaymentProcessorLogEntry
+
     assert issubclass(PseudoPaymentProcessorLogEntry, BaseLogEntry)
     assert isinstance(PseudoPaymentProcessorLogEntry(), BaseLogEntry)
