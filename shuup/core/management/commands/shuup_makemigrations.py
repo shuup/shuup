0cc97f5a22dd5baa428846e50b30a0963eb09e0e
Panu Tulimäki <panu@shuup.com>
2016-11-21 14:37:54 -0800

    Core: Introduce Shuup makemigrations
    Shuup makemigrations ignores verbose names and help texts from
    migrations. This is mainly for Shuup core models.

---
+++ shuup/core/management/commands/shuup_makemigrations.py
@@ -0,0 +1,21 @@
+# This file is part of Shuup.
+#
+# Copyright (c) 2012-2016, Shoop Commerce Ltd. All rights reserved.
+#
+# This source code is licensed under the AGPLv3 license found in the
+# LICENSE file in the root directory of this source tree.
+from django.core.management.commands.makemigrations import Command  # noqa
+from django.db import models
+
+original_deconstruct = models.Field.deconstruct
+
+
+def new_deconstruct(self):
+    name, path, args, kwargs = original_deconstruct(self)
+    if 'help_text' in kwargs:
+        del kwargs['help_text']
+    if 'verbose_name' in kwargs:
+        del kwargs['verbose_name']
+    return name, path, args, kwargs
+
+models.Field.deconstruct = new_deconstruct
