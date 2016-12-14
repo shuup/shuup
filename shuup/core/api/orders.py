7439655a00589ffddcae758229b76a66a865c432
Gurch Rai <gurch@shuup.com>
2016-12-13 16:06:41 -0800

    API: Add order creation and status update API
    Allow orders to be created through API using the admin json
    order creator.
    Also allow order status to be set to completed or canceled.
    
    Refs EE-81

--- shuup/core/api/orders.py
+++ shuup/core/api/orders.py
@@ -5,6 +5,8 @@
 #
 # This source code is licensed under the AGPLv3 license found in the
 # LICENSE file in the root directory of this source tree.
+from django.http import JsonResponse
+from django.utils.encoding import force_text
 from django.utils.timezone import now
 from django_filters import DateTimeFilter
 from django_filters.rest_framework import DjangoFilterBackend, FilterSet
@@ -13,8 +15,10 @@ from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from shuup.admin.modules.orders.json_order_creator import JsonOrderCreator
+from shuup.admin.modules.orders.views.edit import encode_address
 from shuup.core.models import (
-    MutableAddress, Order, OrderLine, OrderStatus, Payment
+    Contact, MutableAddress, Order, OrderLine, OrderStatus, Payment, Shop
 )
 from shuup.utils.money import Money
 
@@ -22,11 +26,18 @@ from shuup.utils.money import Money
 class OrderLineSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderLine
+        fields = ("product", "sku", "text", "quantity", "type", "base_unit_price_value", "discount_amount_value")
+
+    def get_fields(self):
+        fields = super(OrderLineSerializer, self).get_fields()
+        fields["text"].required = False
+        return fields
 
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = MutableAddress
+        fields = "__all__"
 
 
 class PaymentSerializer(serializers.ModelSerializer):
@@ -36,27 +47,27 @@ class PaymentSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    lines = OrderLineSerializer(many=True, read_only=True)
+    lines = OrderLineSerializer(many=True)
     billing_address = AddressSerializer(read_only=True)
     shipping_address = AddressSerializer(read_only=True)
     payments = PaymentSerializer(many=True, read_only=True)
 
+    class Meta:
+        model = Order
+        fields = "__all__"
+
     def get_fields(self):
         fields = super(OrderSerializer, self).get_fields()
         for name, field in fields.items():
-            if name in ("status", "key", "label"):
+            if name in ("status", "key", "label", "currency"):
                 field.required = False
             if name == "order_date":
                 field.default = lambda: now()
+            if name == "status":
+                field.default = OrderStatus.objects.get_default_initial()
+            if name in ("shipping_method", "payment_method", "customer"):
+                field.required = True
         return fields
-
-    def create(self, validated_data):
-        if not validated_data.get("status"):
-            validated_data["status"] = OrderStatus.objects.get_default_initial()
-        return super(OrderSerializer, self).create(validated_data)
-
-    class Meta:
-        model = Order
 
 
 class OrderFilter(FilterSet):
@@ -77,6 +88,97 @@ class OrderViewSet(ModelViewSet):
     queryset = Order.objects.all()
     filter_backends = (DjangoFilterBackend,)
     filter_class = OrderFilter
+
+    def create(self, request, *args, **kwargs):
+        text_lines = set()
+        for idx, line in enumerate(request.data.get("lines", [])):
+            if "product" not in line:
+                line["product"] = None
+            if line.get("type") == "text":
+                line["type"] = "other"
+                text_lines.add(idx)
+        request.data["orderer"] = None
+        request.data["modified_by"] = None
+        request.data["creator"] = request.user.pk
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        shop = Shop.objects.get(pk=serializer.data["shop"])
+        customer = Contact.objects.get(pk=serializer.data["customer"])
+        lines = [{
+            "id": (idx + 1),
+            "quantity": line["quantity"],
+            "product": {
+                "id": line["product"]
+            },
+            "baseUnitPrice": line.get("base_unit_price_value"),
+            "unitPrice": line.get("base_unit_price_value") if line["type"].label == "other" else None,
+            "discountAmount": line.get("discount_amount_value", 0),
+            "sku": line.get("sku"),
+            "text": line.get("text"),
+            "type": force_text(line["type"].label) if idx not in text_lines else "text"
+        } for idx, line in enumerate(serializer.data["lines"])]
+
+        data = {
+            "shop": {
+                "selected": {
+                    "id": shop.id,
+                    "name": shop.name,
+                    "currency": shop.currency,
+                    "priceIncludeTaxes": shop.prices_include_tax
+                }
+            },
+            "methods": {
+                "shippingMethod": {"id": serializer.data["shipping_method"]},
+                "paymentMethod": {"id": serializer.data["payment_method"]},
+            },
+            "customer": {
+                "id": serializer.data["customer"],
+                "billingAddress": encode_address(customer.default_billing_address),
+                "shippingAddress": encode_address(customer.default_shipping_address),
+            },
+            "lines": lines
+        }
+        joc = JsonOrderCreator()
+        order = joc.create_order_from_state(data, creator=request.user)
+        if not order:
+            return JsonResponse({
+                "status": "error",
+                "errors": [{
+                    "message": err.message,
+                    "code": err.code
+                } for err in joc.errors]
+            }, status=400)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    @detail_route(methods=['put'])
+    def complete(self, request, pk=None):
+        order = self.get_object()
+        if not order.can_set_complete():
+            return Response({
+                "status": "error",
+                "errors": [{
+                    "message": "Cannot complete order",
+                    "code": "invalid_status_change"
+                }]
+            }, status=status.HTTP_400_BAD_REQUEST)
+        order.status = OrderStatus.objects.get_default_complete()
+        order.save(update_fields=("status",))
+        return Response({"status": "order marked complete"}, status=status.HTTP_200_OK)
+
+    @detail_route(methods=['put'])
+    def cancel(self, request, pk=None):
+        order = self.get_object()
+        if not order.can_set_canceled():
+            return Response({
+                "status": "error",
+                "errors": [{
+                    "message": "Cannot cancel order",
+                    "code": "invalid_status_change"
+                }]
+            }, status=status.HTTP_400_BAD_REQUEST)
+        order.set_canceled()
+        return Response({"status": "order canceled"}, status=status.HTTP_200_OK)
 
     @detail_route(methods=['post'])
     def create_payment(self, request, pk=None):
