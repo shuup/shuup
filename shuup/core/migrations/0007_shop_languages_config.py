2e7920c78af3e293b6a8fa527c350de6ff6284b5
Gurch Rai <gurch@shuup.com>
2016-12-14 17:56:43 -0800

    Core: Fix migration
    Remove explicit Shop model import

--- shuup/core/migrations/0007_shop_languages_config.py
+++ shuup/core/migrations/0007_shop_languages_config.py
@@ -4,11 +4,10 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.db import migrations, models
 from shuup import configuration
-from shuup.core.models import Shop
 
 
 def create_default_shop_languages_config(apps, schema_editor):
-    for shop in Shop.objects.all():
+    for shop in apps.get_model("shuup", "Shop").objects.all():
         if not configuration.get(shop, "languages"):
             configuration.set(shop, "languages", settings.LANGUAGES)
 
