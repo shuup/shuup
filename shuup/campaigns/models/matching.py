1b3b44ce735d5d4ce528808abbe48f0e85cf08a9
Janne Tammi <janne@shuup.com>
2016-12-30 18:11:49 +0200

    Improve category save
    Caches were painfully slowing the save of the product. This Commit should improve the way cache loading is handled and should make product saving a lot faster.

--- shuup/campaigns/models/matching.py
+++ shuup/campaigns/models/matching.py
@@ -4,12 +4,15 @@
 #
 # This source code is licensed under the AGPLv3 license found in the
 # LICENSE file in the root directory of this source tree.
+from django.db.models import Q
 
 from shuup.campaigns.consts import CONTEXT_CONDITION_CACHE_NAMESPACE
 from shuup.campaigns.models import (
-    CatalogFilterCachedShopProduct, ContextCondition
+    CatalogFilter, CatalogFilterCachedShopProduct, CategoryFilter,
+    ContextCondition
 )
 from shuup.core import cache
+from shuup.core.models import ShopProduct
 from shuup.core.utils import context_cache
 
 
@@ -29,15 +32,56 @@ def get_matching_context_conditions(context):
     return matching_context_conditions
 
 
-def update_matching_catalog_filters(filter):
-    # first, invalidate existing cache
-    CatalogFilterCachedShopProduct.objects.filter(filter=filter).delete()
+def update_matching_category_filters(shop_product, ids):
+    filters = CategoryFilter.objects.filter(categories__id__in=ids)
+    q = _get_filter_query(shop_product)
+    all_shop_products = [shop_product] + list(ShopProduct.objects.filter(q))
+    all_ids = [sp.pk for sp in all_shop_products]
 
-    # then add new items in
-    for matching_product in filter.get_matching_shop_products():
-        CatalogFilterCachedShopProduct.objects.create(filter=filter, shop_product=matching_product)
-        context_cache.bump_cache_for_shop_product(matching_product)
+    CatalogFilterCachedShopProduct.objects.filter(filter__in=filters, shop_product__id__in=all_ids).delete()
+
+    for filter in filters:
+        for sp in all_shop_products:
+            if filter.matches(sp):
+                CatalogFilterCachedShopProduct.objects.create(filter=filter, shop_product=sp)
+                context_cache.bump_cache_for_shop_product(sp)  # is this necessary, smells like double
+
+
+def update_matching_catalog_filters(shop_product_or_filter):
+    if isinstance(shop_product_or_filter, CatalogFilter):
+        CatalogFilterCachedShopProduct.objects.filter(filter=shop_product_or_filter).delete()
+        for matching_product in shop_product_or_filter.get_matching_shop_products():
+            CatalogFilterCachedShopProduct.objects.create(filter=shop_product_or_filter, shop_product=matching_product)
+            context_cache.bump_cache_for_shop_product(matching_product)
+        return
+
+    shop_product = shop_product_or_filter
+
+    from shuup.campaigns.utils.matcher import get_matching_for_product
+    q = _get_filter_query(shop_product)
+    all_shop_products = [shop_product] + list(ShopProduct.objects.filter(q))
+    all_ids = [sp.pk for sp in all_shop_products]
+
+    ids = get_matching_for_product(
+        shop_product_or_filter,
+        provide_category="campaign_catalog_filter",
+        skippable_classes=[CatalogFilter]  # these will be handled separately in update_matching_category_filters
+    )
+    CatalogFilterCachedShopProduct.objects.filter(filter__id__in=ids, shop_product__id__in=all_ids).delete()
+    for filter in CatalogFilter.objects.filter(id__in=ids):
+        for sp in all_shop_products:
+            if filter.matches(sp):
+                CatalogFilterCachedShopProduct.objects.create(filter=filter, shop_product=sp)
+                context_cache.bump_cache_for_shop_product(sp)
 
 
 def get_matching_catalog_filters(shop_product):
     return shop_product.cached_catalog_campaign_filters.values_list('filter__id', flat=True)
+
+
+def _get_filter_query(shop_product):
+    q = Q()
+    q |= Q(product__variation_parent_id=shop_product.product)
+    if shop_product.product.variation_parent:
+        q |= Q(product_id=shop_product.product.variation_parent.id)
+    return q
