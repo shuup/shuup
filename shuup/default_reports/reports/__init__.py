744a30d78f1a8f48de302068212e429ddfa47aeb
Christian Hess <christian@shuup.com>
2017-01-11 17:28:48 -0200

    Reports: add new reports
    Add reports to provides
    
    Refs SH-504

--- shuup/default_reports/reports/__init__.py
+++ shuup/default_reports/reports/__init__.py
@@ -5,8 +5,24 @@
 #
 # This source code is licensed under the OSL-3.0 license found in the
 # LICENSE file in the root directory of this source tree.
+from .customer_sales import CustomerSalesReport
+from .new_customers import NewCustomersReport
+from .product_total_sales import ProductSalesReport
+from .refunds import RefundedSalesReport
 from .sales import SalesReport
 from .sales_per_hour import SalesPerHour
+from .shipping import ShippingReport
+from .taxes import TaxesReport
 from .total_sales import TotalSales
 
-__all__ = ["SalesReport", "TotalSales", "SalesPerHour"]
+__all__ = [
+    "CustomerSalesReport",
+    "NewCustomersReport",
+    "ProductSalesReport",
+    "RefundedSalesReport",
+    "SalesPerHour",
+    "SalesReport",
+    "ShippingReport",
+    "TaxesReport",
+    "TotalSales",
+]
