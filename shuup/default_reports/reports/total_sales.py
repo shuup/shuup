a9a0f14bc285e49f3ac6e9c1e9b238c0fcf0632d
Christian Hess <christian@shuup.com>
2017-01-11 17:26:37 -0200

    Reports: total sales show customer average sales
    Refs SH-504

--- shuup/default_reports/reports/total_sales.py
+++ shuup/default_reports/reports/total_sales.py
@@ -5,7 +5,7 @@
 #
 # This source code is licensed under the OSL-3.0 license found in the
 # LICENSE file in the root directory of this source tree.
-from django.db.models import Sum
+from django.db.models import Avg, Count, Sum
 from django.utils.translation import ugettext_lazy as _
 
 from shuup.default_reports.forms import OrderReportForm
@@ -23,15 +23,27 @@ class TotalSales(OrderReportMixin, ShuupReportBase):
         {"key": "name", "title": _("Shop Name")},
         {"key": "currency", "title": _("Currency")},
         {"key": "order_amount", "title": _("Order Amount")},
+        {"key": "customers", "title": _("Customers")},
+        {"key": "customer_avg_sale", "title": _("Customer Average Sale")},
         {"key": "total_sales", "title": _("Total Sales")},
     ]
 
     def get_data(self):
         orders = self.get_objects()
-        total_sales_value = orders.aggregate(total=Sum("taxful_total_price_value"))["total"] or 0
+        aggregation = orders.aggregate(
+            customers=Count("customer", distinct=True),
+            total=Sum("taxful_total_price_value"),
+            avg_sale=Avg("taxful_total_price_value")
+        )
+        total_sales_value = aggregation["total"] or 0
+        customer_avg_sale = aggregation["avg_sale"] or 0
+        customers = aggregation["customers"] or 0
+
         data = [{
             "name": self.shop.name,
             "order_amount": orders.count(),
+            "customers": customers,
+            "customer_avg_sale": self.shop.create_price(customer_avg_sale).as_rounded().value,
             "total_sales": self.shop.create_price(total_sales_value).as_rounded().value,
             "currency": self.shop.currency
         }]
