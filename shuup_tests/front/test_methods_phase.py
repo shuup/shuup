c31b4a627fc7983818e98510c343d7a1457489ca
Janne Tammi <janne@shuup.com>
2017-02-07 10:55:34 +0200

    Fix methods phase test
    Methods phase now requires basket

--- shuup_tests/front/test_methods_phase.py
+++ shuup_tests/front/test_methods_phase.py
@@ -6,13 +6,18 @@
 # LICENSE file in the root directory of this source tree.
 
 import pytest
-
 from bs4 import BeautifulSoup
 from django.utils.translation import activate
 
+from shuup.core.models import get_person_contact
+from shuup.front.basket import get_basket
 from shuup.front.checkout.methods import MethodsPhase
-from shuup.testing.factories import get_payment_method, get_shipping_method, get_default_shop
+from shuup.testing.factories import (
+    create_product, get_default_shop, get_default_supplier, get_payment_method,
+    get_shipping_method
+)
 from shuup.testing.utils import apply_request_middleware
+from shuup_tests.utils import printable_gibberish
 
 SHIPPING_DATA = {
     "name": "Home delivery",
@@ -30,7 +35,7 @@ PAYMENT_DATA = {
     (get_shipping_method, SHIPPING_DATA, "id_shipping_method"),
     (get_payment_method, PAYMENT_DATA, "id_payment_method")
 ])
-def test_method_phase_basic(rf, get_method, data, method_id):
+def test_method_phase_basic(rf, admin_user, get_method, data, method_id):
     activate("en")
     shop = get_default_shop()
     method = get_method(shop, price=0, name=data["name"])
@@ -39,13 +44,33 @@ def test_method_phase_basic(rf, get_method, data, method_id):
     assert method.enabled
 
     view = MethodsPhase.as_view()
+
+    # To make method visible, basket must be available
+    person = get_person_contact(admin_user)
     request = apply_request_middleware(rf.get("/"))
+    request.shop = shop
+    request.user = admin_user
+    request.person = person
+    request.customer = person
+    basket = get_basket(request)
+
+    # add product to bakset
+    supplier = get_default_supplier()
+    product = create_product(printable_gibberish(), shop=shop, supplier=supplier, default_price=50)
+    basket.add_product(supplier=supplier, shop=shop, product=product, quantity=2)
+    assert basket.product_count, "basket has products"
+
+    basket.save()
+    request = apply_request_middleware(request, user=admin_user, person=person, customer=person, basket=basket)
+
+    # request = apply_request_middleware(rf.get("/"))
     response = view(request=request)
     if hasattr(response, "render"):
         response.render()
     assert response.status_code in [200, 302]
     soup = BeautifulSoup(response.content)
     method_soup = soup.find("div", {"id": method_id})
+
     assert data["name"] in method_soup.text
     assert data["description"] in method_soup.text
     assert soup.find("input", {"id": "%s_%s" % (method_id, method.pk)})
