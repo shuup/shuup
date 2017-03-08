e7fac5cdd1216ccb09f2b57e916770f33f39d80d
Gurchet Rai <gurch@shuup.com>
2017-03-08 12:04:30 -0800

    Refactor basket
    Create a Basket model in core and move basket logic
    from front to core so that a basket can be used without
    any need for shuup.front. Unlike the front basket, the core
    basket does not use sessions for basket retrieval but is
    instead reliant on the caller to pass in the `basket.key`.

---
+++ shuup/core/basket/command_dispatcher.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+# This file is part of Shuup.
+#
+# Copyright (c) 2012-2017, Shoop Commerce Ltd. All rights reserved.
+#
+# This source code is licensed under the OSL-3.0 license found in the
+# LICENSE file in the root directory of this source tree.
+from __future__ import unicode_literals
+
+import six
+from django.core.exceptions import ValidationError
+from django.http import HttpResponseRedirect, JsonResponse
+from django.shortcuts import redirect
+from django.utils.encoding import force_text
+from django.utils.translation import ugettext_lazy as _
+
+from shuup.core.basket import commands
+from shuup.core.signals import get_basket_command_handler
+from shuup.utils.excs import Problem
+
+
+class BasketCommandDispatcher(object):
+    """
+    BasketCommandDispatcher handles (usually AJAX) requests that somehow update the basket.
+    You should never instantiate BasketCommandDispatcher yourself -- instead use
+    `get_basket_command_dispatcher()`.
+
+    All `handle_*` methods are expected to accept `**kwargs`.
+    """
+
+    commands_module = commands
+
+    def __init__(self, request, basket=None):
+        """
+        :type request: HttpRequest
+        """
+        self.request = request
+        self.ajax = self.request.is_ajax()
+        # :type self.basket: BaseBasket
+        self.basket = (basket or request.basket)
+
+    def get_command_handler(self, command):
+        handler = getattr(self.commands_module, "handle_%s" % command.lower(), None)
+        if handler and callable(handler):
+            return handler
+
+        for receiver, handler in get_basket_command_handler.send(
+                BasketCommandDispatcher, command=command, instance=self):
+            if handler and callable(handler):
+                return handler
+
+    def handle(self, command, kwargs=None):
+        """
+        Dispatch and handle processing of the given command.
+
+        :param command: Name of command to run
+        :type command: unicode
+        :param kwargs: Arguments to pass to the command handler. If empty, `request.POST` is used.
+        :type kwargs: dict
+        :return: response
+        :rtype: HttpResponse
+        """
+
+        kwargs = kwargs or dict(six.iteritems(self.request.POST))
+        try:
+            handler = self.get_command_handler(command)
+            if not handler or not callable(handler):
+                raise Problem(_(u"Invalid command %s") % command)
+            kwargs.pop("csrfmiddlewaretoken", None)  # The CSRF token should never be passed as a kwarg
+            kwargs.pop("command", None)  # Nor the command
+            kwargs.update(request=self.request, basket=self.basket)
+            kwargs = self.preprocess_kwargs(command, kwargs)
+            response = handler(**kwargs) or {}
+        except (Problem, ValidationError) as exc:
+            if not self.ajax:
+                raise
+            msg = exc.message if hasattr(exc, "message") else exc
+            response = {
+                "error": force_text(msg, errors="ignore"),
+                "code": force_text(getattr(exc, "code", None) or "", errors="ignore")
+            }
+        response = self.postprocess_response(command, kwargs, response)
+
+        if self.ajax:
+            return JsonResponse(response)
+
+        return_url = (response.get("return") or kwargs.get("return"))
+        if return_url and return_url.startswith("/"):
+            return HttpResponseRedirect(return_url)
+        return redirect("shuup:basket")
+
+    def preprocess_kwargs(self, command, kwargs):
+        """
+        Preprocess kwargs before they are passed to the given `command` handler.
+        Useful for subclassing. Must return the new `kwargs`, even if it wasn't
+        mutated.
+
+        :param command: The name of the command about to be run
+        :param kwargs: dict of arguments
+        :return: dict of arguments
+        """
+        return kwargs
+
+    def postprocess_response(self, command, kwargs, response):
+        """
+        Postprocess the response dictionary (not a HTTP response!) before it is
+        either turned into JSON or otherwise processed (in the case of non-AJAX requests).
+
+        :param command: The command that was run.
+        :param kwargs: The actual kwargs the command was run with.
+        :param response: The response the command returned.
+        :return: The response to be processed and sent to the client.
+        """
+        return response
---
+++ shuup/core/models/_basket.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# This file is part of Shuup.
+#
+# Copyright (c) 2012-2017, Shoop Commerce Ltd. All rights reserved.
+#
+# This source code is licensed under the OSL-3.0 license found in the
+# LICENSE file in the root directory of this source tree.
+
+from django.conf import settings
+from django.db import models
+from django.db.models.fields.related import ManyToManyField
+from django.utils.crypto import get_random_string
+from django.utils.translation import ugettext_lazy as _
+
+from shuup.core.fields import CurrencyField, MoneyValueField, TaggedJSONField
+from shuup.utils.properties import (
+    MoneyPropped, TaxfulPriceProperty, TaxlessPriceProperty
+)
+
+
+def generate_key():
+    return get_random_string(32)
+
+
+class Basket(MoneyPropped, models.Model):
+    # A combination of the PK and key is used to retrieve a basket for session situations.
+    key = models.CharField(max_length=32, default=generate_key, verbose_name=_('key'), unique=True, db_index=True)
+
+    shop = models.ForeignKey("Shop", on_delete=models.CASCADE, verbose_name=_('shop'))
+
+    customer = models.ForeignKey(
+        "Contact", blank=True, null=True,
+        on_delete=models.CASCADE,
+        related_name="customer_core_baskets",
+        verbose_name=_('customer')
+    )
+    orderer = models.ForeignKey(
+        "PersonContact", blank=True, null=True,
+        on_delete=models.CASCADE,
+        related_name="orderer_core_baskets",
+        verbose_name=_('orderer')
+    )
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL, blank=True, null=True,
+        on_delete=models.CASCADE,
+        related_name="core_baskets_created",
+        verbose_name=_('creator')
+    )
+
+    created_on = models.DateTimeField(auto_now_add=True, db_index=True, editable=False, verbose_name=_('created on'))
+    updated_on = models.DateTimeField(auto_now=True, db_index=True, editable=False, verbose_name=_('updated on'))
+    persistent = models.BooleanField(db_index=True, default=False, verbose_name=_('persistent'))
+    deleted = models.BooleanField(db_index=True, default=False, verbose_name=_('deleted'))
+    finished = models.BooleanField(db_index=True, default=False, verbose_name=_('finished'))
+    title = models.CharField(max_length=64, blank=True, verbose_name=_('title'))
+    data = TaggedJSONField(verbose_name=_('data'))
+
+    # For statistics etc., as `data` is opaque:
+    taxful_total_price = TaxfulPriceProperty('taxful_total_price_value', 'currency')
+    taxless_total_price = TaxlessPriceProperty('taxless_total_price_value', 'currency')
+
+    taxless_total_price_value = MoneyValueField(default=0, null=True, blank=True, verbose_name=_('taxless total price'))
+    taxful_total_price_value = MoneyValueField(default=0, null=True, blank=True, verbose_name=_('taxful total price'))
+    currency = CurrencyField(verbose_name=_('currency'))
+    prices_include_tax = models.BooleanField(verbose_name=_('prices include tax'))
+
+    product_count = models.IntegerField(default=0, verbose_name=_('product_count'))
+    products = ManyToManyField("Product", blank=True, verbose_name=_('products'))
+
+    class Meta:
+        verbose_name = _('basket')
+        verbose_name_plural = _('baskets')
