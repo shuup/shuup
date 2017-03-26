5fbdcda0eab9a7f33764609d2ae9b41c88decdc9
Janne Tammi <janne@shuup.com>
2017-03-25 19:25:27 -0700

    Basket fixes

--- shuup/core/basket/objects.py
+++ shuup/core/basket/objects.py
@@ -12,6 +12,7 @@ from collections import Counter
 from decimal import Decimal
 
 import six
+from django.contrib import messages
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
@@ -84,9 +85,11 @@ class BasketLine(SourceLine):
 
 
 class BaseBasket(OrderSource):
-    def __init__(self, request):
+    def __init__(self, request, basket_name="basket"):
         super(BaseBasket, self).__init__(request.shop)
         self.request = request
+        self.basket_name = basket_name
+        self.key = basket_name
         if request:
             self.ip_address = request.META.get("REMOTE_ADDR")
         self.storage = get_storage()
@@ -109,8 +112,24 @@ class BaseBasket(OrderSource):
         self._lines_by_line_id_cache = None
 
     def _load(self):
-        # TODO: raise NotImplementedError
-        pass
+        """
+        Get the currently persisted data for this basket.
+        This will only access the storage once per request in usual
+        circumstances.
+        :return: Data dict.
+        :rtype: dict
+        """
+        if self._data is None:
+            try:
+                self._data = self.storage.load(basket=self)
+            except BasketCompatibilityError as error:
+                msg = _("Basket loading failed: Incompatible basket (%s)")
+                messages.error(self.request, msg % error)
+                self.storage.delete(basket=self)
+                self._data = self.storage.load(basket=self)
+            self.dirty = False
+            self.uncache()
+        return self._data
 
     def save(self):
         """
