b024c835e67a36d70ccdb44cccb4058b655f7239
Christian Hess <christian@shuup.com>
2017-04-12 09:26:51 -0300

    API: Remove dead assigment

--- shuup/core/basket/objects.py
+++ shuup/core/basket/objects.py
@@ -52,8 +52,7 @@ class BasketLine(SourceLine):
         self.gross_weight = product.gross_weight
         self.shipping_mode = product.shipping_mode
         self.sku = product.sku
-        shop_product = self.shop_product
-        self.text = shop_product.safe_translation_getter("name", any_language=True)
+        self.text = self.shop_product.safe_translation_getter("name", any_language=True)
 
         if not self.text:
             self.text = product.safe_translation_getter("name", any_language=True)
