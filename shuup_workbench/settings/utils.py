8f3e54c3c15d42cd66d5c2ad3da57d08b74a13f0
Christian Hess <christianhess.rlz@gmail.com>
2018-03-10 22:13:52 -0300

    Changed the way migrations are ignored

--- shuup_workbench/settings/utils.py
+++ shuup_workbench/settings/utils.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
 
 
-class DisableMigrations(object):
-    # See https://gist.github.com/NotSqrt/5f3c76cd15e40ef62d09
-    def __contains__(self, item):
-        return True
+def get_disabled_migrations():
+    return {
+        'auth': None,
+        'contenttypes': None,
+        'default': None,
+        'sessions': None,
 
-    def __getitem__(self, item):
-        return "notmigrations"
+        'shuup': None,
+        'shuup_admin': None,
+        'default_tax': None,
+        'shuup_front': None,
+        'carousel': None,
+        'shuup_notify': None,
+        'shuup_simple_cms': None,
+        'simple_supplier': None,
+        'shuup_customer_group_pricing': None,
+        'campaigns': None,
+        'shuup_xtheme': None,
+        'shuup_testing': None,
+
+        'django_countries': None,
+        'filer': None,
+    }
