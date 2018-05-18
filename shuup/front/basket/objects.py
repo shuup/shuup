6ed17265900fa5bef6908f60128628879293c046
Christian Hess <christianhess.rlz@gmail.com>
2018-05-18 09:58:03 -0300

    Front: add shop option to require payment and shipping methods on checkout

--- shuup/front/basket/objects.py
+++ shuup/front/basket/objects.py
@@ -8,10 +8,15 @@
 from __future__ import unicode_literals
 
 from django.contrib import messages
+from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
+from shuup import configuration
 from shuup.core.basket.objects import BaseBasket as Basket
 from shuup.core.basket.storage import BasketCompatibilityError
+from shuup.front.checkout.methods import (
+    PAYMENT_METHOD_REQUIRED_CONFIG_KEY, SHIPPING_METHOD_REQUIRED_CONFIG_KEY
+)
 
 
 class BaseBasket(Basket):
@@ -38,3 +43,20 @@ class BaseBasket(Basket):
             self.dirty = False
             self.uncache()
         return self._data
+
+    def get_methods_validation_errors(self):
+        shipping_methods = self.get_available_shipping_methods()
+        payment_methods = self.get_available_payment_methods()
+
+        advice = _(
+            "Try to remove some products from the basket "
+            "and order them separately.")
+
+        if (self.has_shippable_lines() and not shipping_methods and
+                configuration.get(self.shop, SHIPPING_METHOD_REQUIRED_CONFIG_KEY, True)):
+            msg = _("Products in basket cannot be shipped together. %s")
+            yield ValidationError(msg % advice, code="no_common_shipping")
+
+        if not payment_methods and configuration.get(self.shop, PAYMENT_METHOD_REQUIRED_CONFIG_KEY, True):
+            msg = _("Products in basket have no common payment method. %s")
+            yield ValidationError(msg % advice, code="no_common_payment")
