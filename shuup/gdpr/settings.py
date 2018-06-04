76e2de1eeace29fb4c9d4d546cb1cbbaf68f9590
Janne Tammi <janne@shuup.com>
2018-06-04 16:52:34 -0700

    GDPR: Ensure sane defaults

--- shuup/gdpr/settings.py
+++ shuup/gdpr/settings.py
@@ -7,5 +7,26 @@
 # LICENSE file in the root directory of this source tree.
 from __future__ import unicode_literals
 
+from django.utils.translation import ugettext_lazy as _
+
 # The cookie name that will save the consent in the user browser
 SHUUP_GDPR_CONSENT_COOKIE_NAME = "shuup_gdpr_consent"
+
+
+SHUUP_GDPR_DEFAULT_BANNER_STRING = _("<p>This website stores cookies on your computer. "
+                                     "These cookies are used to collect information about how you interact with our "
+                                     "website and allow us to remember you. We use this information in order to "
+                                     "improve and customize your browsing experience and for analytics and metrics "
+                                     "about our visitors both on this website and other media. To find out more "
+                                     "about the cookies we use, see our Privacy Policy.</p>")
+
+
+SHUUP_GDPR_DEFAULT_EXCERPT_STRING = _("<p>When you visit any website, it may store or retrieve information on your"
+                                      "browser, mostly in the form of cookies. This information might be about you, "
+                                      "your preferences or your device and is mostly used to make the site work as "
+                                      "you expect it to. The information does not usually directly identify you, "
+                                      "but it can give you a more personalized web experience.</p>"
+                                      "<p>Because we respect your right to privacy, you can choose not to allow some "
+                                      "types of cookies. Click on the different category headings to find out more "
+                                      "and change our default settings. However, blocking some types of cookies may "
+                                      "impact your experience of the site and the services we are able to offer.</p>")
