1199988a1674b58aecc0fd131e3816e11436d39f
Janne Tammi <janne@shuup.com>
2018-06-21 09:03:24 -0700

    GDPR: Fix migrations
    Changing many to many target from CMS page to
    GDPR document caused issues with the migrations
    since the swap possibly caused integrity errors
    with db. Adding new field and then by renaming
    after the migration this issue should go away.

--- shuup/gdpr/migrations/0002_consent_document.py
+++ shuup/gdpr/migrations/0002_consent_document.py
@@ -6,6 +6,39 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
+def forwards_func(apps, schema_editor):
+    """
+    Convert pages to GDPR user consent documents
+    """
+    user_consent_document_model = apps.get_model("shuup_gdpr", "GDPRUserConsentDocument")
+    gdpr_user_consent_model = apps.get_model("shuup_gdpr", "GDPRUserConsent")
+    reversion_version_model = apps.get_model("reversion", "Version")
+
+    for consent in gdpr_user_consent_model.objects.all():
+        consents = set()
+        for page_id in consent.documents.values_list("id", flat=True):
+            version = reversion_version_model.objects.filter(object_id=page_id).first()
+            if version:
+                doc = user_consent_document_model.objects.create(page_id=page_id, version=version)
+                consents.add(doc.id)
+
+        consent.consents = consents
+
+
+def backwards_func(apps, schema_editor):
+    """
+    Convert GDPR user consent documents to pages
+    """
+    gdpr_user_consent_model = apps.get_model("shuup_gdpr", "GDPRUserConsent")
+
+    for consent in gdpr_user_consent_model.objects.all():
+        page_ids = set()
+        for consent_document in consent.consents.values_list("id", flat=True):
+            page_ids.add(consent_document.page.id)
+
+        consent.documents = page_ids
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -43,9 +76,16 @@ class Migration(migrations.Migration):
             name='gdpruserconsent',
             options={'verbose_name': 'GDPR user consent', 'verbose_name_plural': 'GDPR user consents'},
         ),
-        migrations.AlterField(
+        migrations.AddField(
+            model_name='gdpruserconsent',
+            name='consents',
+            field=models.ManyToManyField(
+                blank=True, editable=False, to='shuup_gdpr.GDPRUserConsentDocument', verbose_name='consent documents'
+            )
+        ),
+        migrations.RunPython(forwards_func, backwards_func),
+        migrations.RemoveField(
             model_name='gdpruserconsent',
             name='documents',
-            field=models.ManyToManyField(blank=True, editable=False, to='shuup_gdpr.GDPRUserConsentDocument', verbose_name='consent documents'),
-        ),
+        )
     ]
