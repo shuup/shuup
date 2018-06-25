913fb56a6c7f9f15d81dfa2f1293e8b188ef801b
Christian Hess <christianhess.rlz@gmail.com>
2018-06-25 12:53:40 -0300

    Tests: enable ForeignKey constraint for SQLite database backends
    The constraint is not enabled for browser tests

---
+++ shuup_tests/core/test_fk_constraint.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# This file is part of Shuup.
+#
+# Copyright (c) 2012-2018, Shuup Inc. All rights reserved.
+#
+# This source code is licensed under the OSL-3.0 license found in the
+# LICENSE file in the root directory of this source tree.
+import pytest
+from shuup.core.models import SavedAddress
+from django.db.utils import IntegrityError
+
+
+@pytest.mark.django_db
+def test_fk_constraint():
+    """
+    Test that FK constraint should be on and objects MUST exist
+    """
+    with pytest.raises(IntegrityError):
+        SavedAddress.objects.create(owner_id=78472384723847218, address_id=4723847283)
