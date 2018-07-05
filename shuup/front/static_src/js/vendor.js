282e6fa6d240430ffec38cca5241db4066268d37
Christian Hess <christianhess.rlz@gmail.com>
2018-07-05 00:39:31 -0300

    Build Front static source using Parcel
    Refs #1286

---
+++ shuup/front/static_src/js/vendor.js
@@ -0,0 +1,16 @@
+/**
+ * This file is part of Shuup.
+ *
+ * Copyright (c) 2012-2018, Shuup Inc. All rights reserved.
+ *
+ * This source code is licensed under the OSL-3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+const $ = require("jquery");
+window.$ = window.jQuery = $;
+
+require("bootstrap");
+require("bootstrap-select");
+require("jquery.easing");
+require("owl.carousel");
+require("simplelightbox");
