263f683cf465367d5ae0f0c5fc0fafe75ee55ac1
Tamas <tamas.kertesz@cult.tamk.fi>
2018-07-13 12:28:26 +0300

    Swap gulp for parcel
    Remove gulp and webpack and switch every build process over to parcel

--- shuup/admin/modules/products/static_src/product-variation-variable-editor.js
+++ shuup/admin/modules/products/static_src/product-variation-variable-editor.js
@@ -6,6 +6,10 @@
  * This source code is licensed under the OSL-3.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
+const m = require('mithril');
+window.m = m;
+
 window.VariationVariableEditor = (function(m, _) {
     "use strict";
     var languages = null;
