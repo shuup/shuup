5df6478dc749c986cd2633f02295a3714ac0f848
Panu Tulimäki <panu@shuup.com>
2018-07-18 10:43:53 -0700

    Filter suppliers based on current shop
    Refs #1379

--- shuup/admin/modules/suppliers/views/list.py
+++ shuup/admin/modules/suppliers/views/list.py
@@ -8,8 +8,10 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
+from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
 
+from shuup.admin.shop_provider import get_shop
 from shuup.admin.toolbar import Toolbar
 from shuup.admin.utils.picotable import Column, TextFilter
 from shuup.admin.utils.views import PicotableListView
@@ -32,6 +34,9 @@ class SupplierListView(PicotableListView):
         Column("type", _(u"Type")),
         Column("module_identifier", _(u"Module"), display="get_module_display", sortable=True)
     ]
+
+    def get_queryset(self):
+        return Supplier.objects.filter(Q(shops=get_shop(self.request)) | Q(shops__isnull=True))
 
     def get_module_display(self, instance):
         return instance.module.name or _("No %s module") % self.model._meta.verbose_name
