ab5b53f35dc83f3802bdef4d157225d2d7a1add2
tmskrtsz <tamas.kertesz@cult.tamk.fi>
2018-08-14 11:05:18 +0300

    Fix dropzone in media browser

--- shuup/admin/static_src/product/edit_media.js
+++ shuup/admin/static_src/product/edit_media.js
@@ -6,6 +6,9 @@
  * This source code is licensed under the OSL-3.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
+import { activateDropzone } from '../base/js/dropzone';
+
 $(function() {
     $(".product-media-delete").on("click", function(e) {
         e.preventDefault();
