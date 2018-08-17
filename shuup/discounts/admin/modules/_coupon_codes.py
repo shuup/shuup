ef2252fe1f2ea006f635b0e47a824070cc42b4e1
Panu Tulimäki <panu@shuup.com>
2018-08-17 16:13:33 -0700

    Discounts: Hotfix discount module imports

--- shuup/discounts/admin/modules/_coupon_codes.py
+++ shuup/discounts/admin/modules/_coupon_codes.py
@@ -11,7 +11,6 @@ from django.utils.translation import ugettext_lazy as _
 
 from shuup.admin.base import AdminModule, MenuEntry
 from shuup.admin.menu import CAMPAIGNS_MENU_CATEGORY
-from shuup.admin.urls import admin_url
 from shuup.admin.utils.permissions import get_default_model_permissions
 from shuup.admin.utils.urls import derive_model_url, get_edit_and_list_urls
 from shuup.discounts.models import CouponCode
@@ -22,6 +21,7 @@ class CouponCodeModule(AdminModule):
     breadcrumbs_menu_entry = MenuEntry(name, url="shuup_admin:discounts_coupon_codes.list")
 
     def get_urls(self):
+        from shuup.admin.urls import admin_url
         delete = admin_url(
             "^discounts_coupon_codes/(?P<pk>\d+)/delete/$",
             "shuup.discounts.admin.views.CouponCodeDeleteView",
