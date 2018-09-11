c2b45f332524b7816237a3297755e2b2bc85c1b4
Panu Tulimäki <panu@shuup.com>
2018-09-11 09:15:52 -0700

    GDPR: Fix URLs

--- shuup/gdpr/urls.py
+++ shuup/gdpr/urls.py
@@ -13,9 +13,9 @@ from .views import (
 )
 
 urlpatterns = [
-    url(r'^gdpr/policy-consent/(?P<page_id>\d+)/?$', GDPRPolicyConsentView.as_view(), name='gdpr_policy_consent'),
-    url(r'^gdpr/consent/?$', GDPRCookieConsentView.as_view(), name='gdpr_consent'),
-    url(r'^gdpr/download_data/?$', GDPRDownloadDataView.as_view(), name='gdpr_download_data'),
-    url(r'^gdpr/anonymize/?$', GDPRAnonymizeView.as_view(), name='gdpr_anonymize_account'),
-    url(r'^gdpr/customer/?$', GDPRCustomerDashboardView.as_view(), name='gdpr_customer_dashboard')
+    url(r'^gdpr/policy-consent/(?P<page_id>\d+)/$', GDPRPolicyConsentView.as_view(), name='gdpr_policy_consent'),
+    url(r'^gdpr/consent/$', GDPRCookieConsentView.as_view(), name='gdpr_consent'),
+    url(r'^gdpr/download_data/$', GDPRDownloadDataView.as_view(), name='gdpr_download_data'),
+    url(r'^gdpr/anonymize/$', GDPRAnonymizeView.as_view(), name='gdpr_anonymize_account'),
+    url(r'^gdpr/customer/$', GDPRCustomerDashboardView.as_view(), name='gdpr_customer_dashboard')
 ]
