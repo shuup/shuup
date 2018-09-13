332f0e0e0647f8abd62f9ff3c4054651ec6cd90f
Panu Tulimäki <panu@shuup.com>
2018-09-12 22:33:28 -0700

    Admin: add license header

--- shuup/admin/static_src/base/js/base.js
+++ shuup/admin/static_src/base/js/base.js
@@ -1,3 +1,11 @@
+/**
+ * This file is part of Shuup.
+ *
+ * Copyright (c) 2012-2018, Shuup Inc. All rights reserved.
+ *
+ * This source code is licensed under the OSL-3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import './browse-widget.js';
 import './content-blocks.js';
 import './datetimepicker.js';
