58abc1118192a64f5b524abb064135ffa982ec51
Panu Tulimäki <panu@shuup.com>
2018-09-13 16:42:37 -0700

    Xtheme: fix editor resources

---
+++ shuup/xtheme/static_src/editor/vendor.js
@@ -0,0 +1,21 @@
+/**
+ * This file is part of Shuup.
+ *
+ * Copyright (c) 2012-2018, Shuup Inc. All rights reserved.
+ *
+ * This source code is licensed under the OSL-3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//-- jQuery
+var jquery = require("jquery");
+window.$ = window.jQuery = jquery;
+
+const _ = require('lodash');
+window._ = _;
+
+window.Sortable = require('sortablejs');
+
+require('bootstrap');
+require("summernote/dist/summernote.js");
+require("../../../admin/static_src/base/js/browse-widget.js");
