dfbbb7db29e10c05d650be0f40dd0f67f15ee46a
Panu Tulimäki <panu@shuup.com>
2018-09-13 09:13:46 -0700

    Testing: do not sleep in browser test init

--- shuup/testing/utils.py
+++ shuup/testing/utils.py
@@ -6,7 +6,6 @@
 # This source code is licensed under the OSL-3.0 license found in the
 # LICENSE file in the root directory of this source tree.
 import inspect
-import time
 
 from django.conf import settings
 from django.core import urlresolvers
@@ -138,6 +137,5 @@ def initialize_admin_browser_test(
         # set shop language to eng
         browser.find_by_id("dropdownMenu").click()
         browser.find_by_xpath('//a[@data-value="%s"]' % language).first.click()
-        time.sleep(1)
 
     return browser
