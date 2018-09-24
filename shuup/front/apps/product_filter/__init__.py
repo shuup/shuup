451c67587a7ca83d3d4b06a0f89c2d1f496c11d3
soulilya <ghostriderilya@mail.ru>
2018-09-25 01:17:07 +0300

    Revert "Product filter for shuup with fixes"
    This reverts commit 926046d4803cd24dea1c225b23b15cc75cabd5a1.

--- shuup/front/apps/product_filter/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# This file is part of Shuup.
-#
-# Copyright (c) 2012-2018, Shuup Inc. All rights reserved.
-#
-# This source code is licensed under the OSL-3.0 license found in the
-# LICENSE file in the root directory of this source tree.
-default_app_config = __name__ + ".apps.AppConfig"
