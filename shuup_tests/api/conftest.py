6805cf8b7b30f8d15dfd1192b3c6473950e41102
chessbr <christianhess.rlz@gmail.com>
2018-10-02 16:19:04 -0300

    Fix unit test by adding back front apps after API tests

--- shuup_tests/api/conftest.py
+++ shuup_tests/api/conftest.py
@@ -8,5 +8,14 @@
 from django.conf import settings
 
 
+ORIGINAL_SETTINGS = []
+
+
 def pytest_runtest_setup(item):
+    global ORIGINAL_SETTINGS
+    ORIGINAL_SETTINGS = [item for item in settings.INSTALLED_APPS]
     settings.INSTALLED_APPS = [app for app in settings.INSTALLED_APPS if "shuup.front" not in app]
+
+
+def pytest_runtest_teardown(item):
+    settings.INSTALLED_APPS = [item for item in ORIGINAL_SETTINGS]
