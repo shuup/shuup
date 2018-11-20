8414fbc7fdcb987f075670829d4c15bcca18f112
Panu Tulimäki <panu@shuup.com>
2018-11-20 08:33:27 -0800

    Release 1.7.3b2

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ VERSION_FILE = os.path.join(TOPDIR, 'shuup', '_version.py')
 #      - Add ".post0.dev" suffix to VERSION variable here
 
 NAME = 'shuup'
-VERSION = '1.7.3b1.post0.dev'
+VERSION = '1.7.3b2'
 DESCRIPTION = 'E-Commerce Platform'
 AUTHOR = 'Shuup Inc.'
 AUTHOR_EMAIL = 'shuup@shuup.com'
