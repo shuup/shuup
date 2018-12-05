c1751cb3ecaa0d06f1ce42a2a6d72e1fc90e282b
chessbr <christianhess.rlz@gmail.com>
2018-12-05 10:55:20 -0200

    Front: add option to remove codes from the basket
    Add list of used codes in basket and button to remove them
    
    Fixes #1653

--- shuup/front/basket/commands.py
+++ shuup/front/basket/commands.py
@@ -7,5 +7,5 @@
 # LICENSE file in the root directory of this source tree.
 from shuup.core.basket.commands import (  # noqa
     handle_add, handle_add_campaign_code, handle_add_var, handle_clear,
-    handle_del, handle_update
+    handle_del, handle_remove_campaign_code, handle_update
 )
