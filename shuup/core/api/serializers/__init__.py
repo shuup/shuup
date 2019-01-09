12f3852596c68d3a235cefbfa47e6fe33ff7e0d4
Christian Hess <christianhess.rlz@gmail.com>
2019-01-09 14:21:43 -0200

    API: add more fields in Order serializers
    No Refs

---
+++ shuup/core/api/serializers/__init__.py
@@ -0,0 +1,11 @@
+# This file is part of Shuup.
+#
+# Copyright (c) 2012-2018, Shuup Inc. All rights reserved.
+#
+# This source code is licensed under the OSL-3.0 license found in the
+# LICENSE file in the root directory of this source tree.
+from .label import LabelSerializer
+
+__all__ = [
+    "LabelSerializer"
+]
