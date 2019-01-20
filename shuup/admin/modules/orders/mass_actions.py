6ec782eb1bf1ab793e93bc41ab9607de80cd1586
Lav Filip <filip@shuup.com>
2019-01-20 16:14:27 +0100

    Bump product cache when using mass action, limit mass action changes to one shop
    .update() is a direct SQL statement so it wasn't calling save() at all, hence it didn't trigger post_save signals at all.
    This iterates through the whole query and saves changes separately for each item. (Recommended method by Django docs)
    
    Refs #1710

--- shuup/admin/modules/orders/mass_actions.py
+++ shuup/admin/modules/orders/mass_actions.py
@@ -14,6 +14,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils.translation import ugettext
 from six import BytesIO
 
+from shuup.admin.shop_provider import get_shop
 from shuup.admin.utils.picotable import (
     PicotableFileMassAction, PicotableMassAction
 )
@@ -28,9 +29,11 @@ class CancelOrderAction(PicotableMassAction):
     identifier = "mass_action_order_cancel"
 
     def process(self, request, ids):
-        query = Q(id__in=ids)
+        shop = get_shop(request)
         if isinstance(ids, six.string_types) and ids == "all":
-            query = Q()
+            query = Q(shop=shop)
+        else:
+            query = Q(id__in=ids, shop=shop)
         for order in Order.objects.filter(query):
             if not order.can_set_canceled():
                 continue
