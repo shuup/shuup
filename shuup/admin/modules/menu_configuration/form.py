d47c3ef113ea43a6cdec3d203b940bb4e8deeac5
Ben Deltenre <benjdelt@gmail.com>
2019-02-25 16:09:03 -0800

    Set up configuration form

--- shuup/admin/modules/menu_configuration/form.py
+++ shuup/admin/modules/menu_configuration/form.py
@@ -1,4 +1,17 @@
+import six
 from django import forms
+from shuup.configuration import set, get
+from shuup.admin.module_registry import get_modules
+
 
 class ConfigurationForm(forms.Form):
     name = forms.CharField()
+
+    def __init__(self, **kwargs):
+        # self.shop = kwargs.pop("shop")
+        modules = list(get_modules())
+        super(ConfigurationForm, self).__init__(**kwargs)
+        for module in modules:
+            print(module.name)
+        # for form_field, conf_key in six.iteritems(modules):
+        #     self.initial[form_field] = configuration.get(self.shop, conf_key)
--- shuup/admin/modules/menu_configuration/views/configuration.py
+++ shuup/admin/modules/menu_configuration/views/configuration.py
@@ -2,9 +2,11 @@ from django.views.generic import FormView
 from django.http import HttpResponse
 from shuup.admin.modules.menu_configuration.form import ConfigurationForm
 
+
 class MenuConfigurationFormView(FormView):
     template_name = "shuup/admin/menu_configuration/form.jinja"
     form_class = ConfigurationForm
 
-    # def get(self, request):
-    #     return HttpResponse('Menu Configuration')
+    def get_form_kwargs(self):
+        kwargs = super(MenuConfigurationFormView, self).get_form_kwargs()
+        return kwargs
