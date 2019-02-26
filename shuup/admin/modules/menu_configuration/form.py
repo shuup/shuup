9c57d514a2ff5a8dac7ade6acd77c13107d9502c
Ben Deltenre <benjdelt@gmail.com>
2019-02-25 16:49:27 -0800

    wip

--- shuup/admin/modules/menu_configuration/form.py
+++ shuup/admin/modules/menu_configuration/form.py
@@ -11,7 +11,7 @@ class ConfigurationForm(forms.Form):
         # self.shop = kwargs.pop("shop")
         modules = list(get_modules())
         super(ConfigurationForm, self).__init__(**kwargs)
-        for module in modules:
-            print(module.name)
+        # for module in modules:
+        #     print(module.name)
         # for form_field, conf_key in six.iteritems(modules):
         #     self.initial[form_field] = configuration.get(self.shop, conf_key)
