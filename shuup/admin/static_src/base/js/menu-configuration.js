eaedeb1e5fe3bacd9499c83f7a1e6d442f08ad29
Ben Deltenre <benjdelt@gmail.com>
2019-02-28 09:15:02 -0800

    wip

--- shuup/admin/static_src/base/js/menu-configuration.js
+++ shuup/admin/static_src/base/js/menu-configuration.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+
+    $("button").on("click", function() {
+        console.log($(this).find(".fa-caret-down"));
+    })
+
     html5sortable(".sortable", {
         placeholder: "<div class=\"card\"></div>",
         forcePlaceholderSize: true,
@@ -8,26 +13,18 @@ $(document).ready(function() {
         },
         itemSerializer: function(serializedItem, sortableContainer) {
             var childrenList = $(serializedItem.node).children(".card");
-            // var serializedChildren = [];
-            // for (var i = 0; i < childrenList.length; i += 1) {
-            //     var serialized = html5sortable(childrenList[i], "serialize");
-            //     if (serialized && serialized.length && serialized[0].items.length) {
-            //         serializedChildren.push(serialized[0]);
-            //     }
-            // }
             var node = $(serializedItem.node);
             return {
                 name: node.data("name"),
                 visible_in_menu: node.data("visible-in-menu"),
                 position: serializedItem.index,
-                // children: serializedChildren,
                 parent: serializedItem.parent.parentNode.parentNode.dataset.name,
             }
         }
     });
     $("#menu-configuration-save").on("click", function() {
         var serialized = html5sortable(".sortable", "serialize");
-        console.log(serialized);
+        // console.log(serialized);
         
     })
 });
