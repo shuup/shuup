e47c0a7e8c1a5d9a3d7f4313d5eb7fba2e8b808b
Janne Tammi <janne@shuup.com>
2019-03-20 13:51:47 -0700

    Classic Gray: Add variation button option

--- shuup/front/static_src/js/custom.js
+++ shuup/front/static_src/js/custom.js
@@ -9,3 +9,26 @@
 $(function() {
     $(".selectpicker").selectpicker();
 });
+
+$(document).ready(function() {
+    $(".btn-variation").on("click", function (e) {
+        e.preventDefault();
+        var level = $(this).data("level");
+        $(".btn-variation").each(function (i, elem) {
+            if ($(elem).data("level") === level) {
+                $(elem).removeClass("btn-active");
+            }
+        });
+        $(this).addClass("btn-active");
+        var productId = $(this).data("target-product");
+        var variationId = $(this).data("product-id");
+        var parentProduct = $(this).data("primary-product");
+        if ($("#var_" + productId).length) {
+            $("#var_" + productId).val(variationId);
+        } else {
+            $("#product-variations-" + productId).val(variationId);
+        }
+        var id = (parentProduct) ? parentProduct : productId;
+        window.updatePrice(id);
+    });
+});
