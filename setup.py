5e5a91ea69c56815bb4d8540180fb3fe0b2ac8b8
Panu Tulimäki <panu@shuup.com>
2019-06-04 08:17:10 -0700

    Post-release update for 1.9.5b8

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ VERSION_FILE = os.path.join(TOPDIR, 'shuup', '_version.py')
 #      - Add ".post0.dev" suffix to VERSION variable here
 
 NAME = 'shuup'
-VERSION = '1.9.5b8'
+VERSION = '1.9.5b8.post0.dev'
 DESCRIPTION = 'E-Commerce Platform'
 AUTHOR = 'Shoop Commerce Ltd.'
 AUTHOR_EMAIL = 'shuup@shuup.com'
