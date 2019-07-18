06758b5c42d873e91bf3caa5afa237fea6ab15bf
Christian Hess <christianhess.rlz@gmail.com>
2019-07-18 14:46:13 -0300

    Update summernote version to latest and enable more editor features

--- shuup/admin/static_src/base/js/editor.js
+++ shuup/admin/static_src/base/js/editor.js
@@ -15,21 +15,20 @@ const getMediaButton = ($editor) => context => {
 
     return (
         $.summernote.ui.button({
-            contents: $.summernote.ui.icon($.summernote.options.icons.picture),
-            // tooltip: $.summernote.lang[$.summernote.options.lang].image.image, //Temporarily disabled
+            contents: "<i class=\"fa fa-file-image-o\"/>",
+            tooltip: gettext("Browse media"),
             click() {
-                    window.BrowseAPI.openBrowseWindow({
-                        kind: "media",
-                        clearable: true,
-                        onSelect: (obj) => {
-                            $editor.summernote("insertImage", obj.url);
-                        }
-                    });
-                }
+                window.BrowseAPI.openBrowseWindow({
+                    kind: "media",
+                    clearable: true,
+                    onSelect: (obj) => {
+                        $editor.summernote("insertImage", obj.url);
+                    }
+                });
             }
-        ).render()
+        }).render()
     );
-}
+};
 
 function activateEditor($editor, attrs = {}) {
     function cancelEvent(event) {
@@ -38,9 +37,11 @@ function activateEditor($editor, attrs = {}) {
     }
 
     const toolbar = [
-      ["style", ["style", "bold", "italic", "underline", "clear"]],
-      ["color", ["color"]],
-      ["para", ["ul", "ol", "paragraph"]],
+      ["style", ["style", "clear"]],
+      ["font", ["fontname", "fontsize"]],
+      ["style2", ["bold", "italic", "underline", "strikethrough", "superscript", "subscript"]],
+      ["color", ["forecolor", "backcolor"]],
+      ["para", ["ul", "ol", "paragraph", "height"]],
       ["table", ["table"]],
       ["insert", ["link", "media", "picture", "video", "codeview"]],
       [$.summernote.options.toolbar.filter((option => option[0] !== "insert")).concat([
