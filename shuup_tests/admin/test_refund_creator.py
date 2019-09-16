9459d33bd2d60f67fe7a9c217dfc5faef15f2fe0
Panu Tulimäki <panu@shuup.com>
2019-09-16 10:42:07 +0300

    Admin: add tests around refund view

--- shuup_tests/admin/test_refund_creator.py
+++ shuup_tests/admin/test_refund_creator.py
@@ -8,16 +8,17 @@
 
 import pytest
 
+from bs4 import BeautifulSoup
 from django.test import override_settings
 from django.utils.text import force_text
 
 from shuup.admin.modules.orders.views.refund import (
     OrderCreateFullRefundView, OrderCreateRefundView
 )
-from shuup.core.models import OrderLineType
+from shuup.core.models import OrderLine, OrderLineType
 from shuup.testing.factories import (
-    create_order_with_product, create_product, get_default_shop,
-    get_default_supplier
+    add_product_to_order, create_empty_order, create_order_with_product,
+    create_product, get_default_shop, get_default_supplier
 )
 from shuup.testing.utils import apply_request_middleware
 
@@ -112,3 +113,51 @@ def test_arbitrary_refund_availability(rf, admin_user):
     assert refund_option_str in get_refund_view_content()
     with override_settings(SHUUP_ALLOW_ARBITRARY_REFUNDS=False):
         assert refund_option_str not in get_refund_view_content()
+
+
+@pytest.mark.django_db
+def test_order_refunds_with_other_lines(rf, admin_user):
+    shop = get_default_shop()
+    supplier = get_default_supplier()
+    supplier.shops.add(shop)
+
+    product = create_product("sku", shop=shop, default_price=10)
+    shop_product = product.get_shop_instance(shop=shop)
+    shop_product.suppliers = [supplier]
+
+    order = create_empty_order(shop=shop)
+    order.full_clean()
+    order.save()
+
+    add_product_to_order(order, supplier, product, 4, 5)
+
+    # Lines without quantity shouldn't affect refunds
+    other_line = OrderLine(
+        order=order, type=OrderLineType.OTHER, text="This random line for textual information", quantity=0)
+    other_line.save()
+    order.lines.add(other_line)
+
+    # Lines with quantity again should be able to be refunded normally.
+    other_line_with_quantity = OrderLine(
+        order=order, type=OrderLineType.OTHER, text="Special service 100$/h", quantity=1, base_unit_price_value=100)
+    other_line_with_quantity.save()
+    order.lines.add(other_line_with_quantity)
+
+    assert other_line_with_quantity.max_refundable_quantity == 1
+    assert other_line_with_quantity.max_refundable_amount.value == 100
+
+    order.cache_prices()
+    order.create_payment(order.taxful_total_price)
+    assert order.is_paid()
+    assert order.taxful_total_price_value == 120 # 100 + 4 * 20
+
+    def get_refund_view_content():
+        request = apply_request_middleware(rf.get("/"), user=admin_user)
+        view = OrderCreateRefundView.as_view()
+        response = view(request, pk=order.pk)
+        return BeautifulSoup(response.render().content)
+
+    refund_soup = get_refund_view_content()
+    refund_options = refund_soup.find(id="id_form-0-line_number").findAll("option")
+    assert len(refund_options) == 4  # 1 empty line, 1 for arbitrary and 2 for lines
+    assert len([option for option in refund_options if "Special service 100$/h" in force_text(option)]) == 1
