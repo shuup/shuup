57fb0340ce92bf4e7b9416f637c7974f88e1aa98
maximlion <filip@shuup.com>
2019-10-18 18:19:47 +0200

    Core: Add orderline behaviour attribute
    Added orderline behaviour attribute
    INHERIT = 1 # Inherits the quantity of the parent line if parent line exists
    SKIP = 2 # Skips quantity change upon parent line quantity change
    DELETE = 3 # Removes the line upon parent line quantity change
    
    Refs HEAL-37

--- shuup/core/order_creator/__init__.py
+++ shuup/core/order_creator/__init__.py
@@ -10,7 +10,9 @@ from shuup.utils import update_module_attributes
 
 from ._creator import OrderCreator
 from ._modifier import OrderModifier
-from ._source import OrderSource, SourceLine, TaxesNotCalculated
+from ._source import (
+    OrderLineBehavior, OrderSource, SourceLine, TaxesNotCalculated
+)
 from ._source_modifier import (
     get_order_source_modifier_modules, is_code_usable,
     OrderSourceModifierModule
@@ -31,7 +33,8 @@ __all__ = [
     "OrderSourceMinTotalValidator",
     "OrderSourceSupplierValidator",
     "SourceLine",
-    "TaxesNotCalculated"
+    "TaxesNotCalculated",
+    "OrderLineBehavior"
 ]
 
 update_module_attributes(__all__, __name__)
