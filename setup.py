ae65d17e39c3ec5e647bfc41b22ff0495b611929
Panu Tulimäki <panu@shuup.com>
2019-11-12 11:27:26 +0200

    Post-release update for 1.9.13

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ VERSION_FILE = os.path.join(TOPDIR, 'shuup', '_version.py')
 #      - Add ".post0.dev" suffix to VERSION variable here
 
 NAME = 'shuup'
-VERSION = '1.9.13'
+VERSION = '1.9.13.post0.dev'
 DESCRIPTION = 'E-Commerce Platform'
 AUTHOR = 'Shoop Commerce Ltd.'
 AUTHOR_EMAIL = 'shuup@shuup.com'
