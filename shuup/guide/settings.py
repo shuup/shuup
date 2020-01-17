e31d625fbf9e21c773e405c9506a1cf8b8b14171
Mikko Vedru <mikko.vedru@gmail.com>
2020-01-17 15:53:33 +0200

    Improve settings.py help texts and descriptions

--- shuup/guide/settings.py
+++ shuup/guide/settings.py
@@ -10,20 +10,20 @@
 #: URL for fetching search results via ReadtheDocs API.
 SHUUP_GUIDE_API_URL = "https://readthedocs.org/api/v2/search/?project=shoop-guide&version=latest&"
 
-#: ReadtheDocs link URL
+#: ReadtheDocs link URL.
 #:
 #: URL for manually linking search query link. Query parameters are
 #: added to end of URL when constructing link.
 SHUUP_GUIDE_LINK_URL = "http://shuup-guide.readthedocs.io/en/latest/search.html?check_keywords=yes&area=default&"
 
-#: Whether or not to fetch search results from ReadtheDocs
+#: Whether or not to fetch search results from ReadtheDocs.
 #:
 #: If true, fetch results via the ReadtheDocs API, otherwise only
-#: display a link to RTD search page.
+#: display a link to the RTD search page.
 SHUUP_GUIDE_FETCH_RESULTS = True
 
-#: Timeout limit for fetching search results
+#: Timeout limit for fetching search results.
 #:
 #: Time limit in seconds before a search result request should
-#: timeout, so as not to block search results in case of slow response.
+#: timeout, so as not to block other search results in case of slow response.
 SHUUP_GUIDE_TIMEOUT_LIMIT = 2
--- shuup/xtheme/settings.py
+++ shuup/xtheme/settings.py
@@ -6,24 +6,24 @@
 # LICENSE file in the root directory of this source tree.
 
 
-#: Spec string for the Xtheme admin theme context
+#: Spec string for the Xtheme admin theme context.
 #:
 #: You can use this to determine logic around which themes
-#: are visible in your project admin. This function takes shop
+#: are visible in your project Admin Panel. This function takes shop
 #: `shuup.core.models.Shop` and should return `current_theme_classes`
-#: and `current_theme` for context where `current_theme_classes`
+#: and `current_theme` for context, where `current_theme_classes`
 #: is a list of `shuup.xtheme.models.ThemeSettings`.
 SHUUP_XTHEME_ADMIN_THEME_CONTEXT = (
     "shuup.xtheme.admin_module.utils.get_theme_context"
 )
 
-#: Spec to control Xtheme resource injections
+#: Spec to control Xtheme resource injections.
 #:
 #: Include your template names here to prevent xtheme
 #: injecting resources. This does not expect the template
 #: to exist.
 #:
-#: Can be useful in situations when you have html and body
+#: Can be useful in situations, where you have `html` and `body`
 #: HTML tags inside the actual template structure.
 SHUUP_XTHEME_EXCLUDE_TEMPLATES_FROM_RESOUCE_INJECTION = [
     "notify/admin/script_item_editor.jinja",
