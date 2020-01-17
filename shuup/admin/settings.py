2457c60926bc70b8bec3c96440129803ae5b923a
Mikko Vedru <mikko.vedru@gmail.com>
2020-01-17 15:53:33 +0200

    Improve settings.py help texts and descriptions

--- shuup/admin/settings.py
+++ shuup/admin/settings.py
@@ -16,76 +16,77 @@ your own, the :ref:`apps-naming-settings` section is an important read.
 """
 
 #: Spec which defines a list of Wizard Panes to be shown in Shuup Admin
-#: for Shuup initialization and configuration.
+#: during Shuup's initialization and configuration.
 #:
 #: Panes must be subclasses of `shuup.admin.views.WizardPane`.
 #:
 SHUUP_SETUP_WIZARD_PANE_SPEC = []
 
-#: Spec which defines a function that loads and return discovered admin modules.
-#: This function should return a list of `shuup.admin.base.AdminModule`
+#: Spec which defines a function that loads and returns discovered admin modules.
+#: This function should return a list of `shuup.admin.base.AdminModule`.
 #:
 SHUUP_GET_ADMIN_MODULES_SPEC = ("shuup.admin.module_registry.get_admin_modules")
 
 #: Spec which defines the Shop provider.
 #: The shop provider is the interface responsible for fetching and setting
-#: the active shop in admin module
+#: the active shop in the admin module.
 #:
 SHUUP_ADMIN_SHOP_PROVIDER_SPEC = ("shuup.admin.shop_provider.AdminShopProvider")
+
 #: URL address to Shuup Merchant Documentation and Guide.
 #: The URL must end with a slash.
 #:
 SHUUP_ADMIN_MERCHANT_DOCS_PAGE = "https://shuup-guide.readthedocs.io/en/latest/"
 
-#: The minimum number of characters required to start a search
+#: The minimum number of characters required to start a search.
 #:
 SHUUP_ADMIN_MINIMUM_INPUT_LENGTH_SEARCH = 3
 
-#: Spec that defines the Supplier Provider for a given request
+#: Spec that defines the Supplier Provider for a given request.
 #:
 SHUUP_ADMIN_SUPPLIER_PROVIDER_SPEC = (
     "shuup.admin.supplier_provider.DefaultSupplierProvider")
 
-#: The input format to be used in date pickers
+#: The input format to be used in date pickers.
 #:
 SHUUP_ADMIN_DATE_INPUT_FORMAT = "Y-m-d"
 
-#: The input format to be used in datetime pickers
+#: The input format to be used in datetime pickers.
 #:
 SHUUP_ADMIN_DATETIME_INPUT_FORMAT = "Y-m-d H:i"
 
-#: The input format to be used in time pickers
+#: The input format to be used in time pickers.
 #:
 SHUUP_ADMIN_TIME_INPUT_FORMAT = "H:i"
 
-
-#: The input step to be used for time pickers
+#: The input step to be used for time pickers.
 #:
 SHUUP_ADMIN_DATETIME_INPUT_STEP = 15
 
 #: Menu category identifiers that should always activate the
-#: menu item. Useful in case there is need to always open some
+#: menu item. Useful in case there is a need to always open
 #: certain menus.
 SHUUP_ALWAYS_ACTIVE_MENU_CATEGORY_IDENTIFIERS = []
 
-#: Get front URL for admin panel navigation bar. Override
-#: for example for custom domain logic when admin panel is used
-#: from shared marketplace URL.
+#: Get front URL for admin panel navigation bar. Can be useful for example to
+#: override a custom domain logic when admin panel is used
+#: from the shared marketplace URL.
 SHUUP_ADMIN_NAVIGATION_GET_FRONT_URL_SPEC = ("shuup.admin.utils.urls.get_front_url")
 
-#: Indicates which objects select fields should load options asynchronously.
+#: Indicates which objects `select` fields should load options asynchronously.
+#:
 #: When enabled, fields will load options through AJAX requests instead
-#: of generating them while rendering the page. For enviroments with huge amount of options
-#: in fields, like categories, it is best to have this enabled.
-#: When disabled, the options will be rendered on page load whenever it is possible, as
-#: in some features, the asynchronous load is not an option.
+#: of generating them during the initial rendering the page. For enviroments with a
+#: huge amount of options in their fields, like categories, it is best to have this enabled.
+#:
+#: When disabled, the options will be generated during the first page load.
 #:
 SHUUP_ADMIN_LOAD_SELECT_OBJECTS_ASYNC = {
     "categories": True,
     "suppliers": True
 }
 
-#: Indicates the authentication form class that should be used in login views inside Admin
+#: Indicates the authentication form class, which should be used in login views inside Admin.
 #:
 SHUUP_ADMIN_AUTH_FORM_SPEC = ("shuup.admin.forms.EmailAuthenticationForm")
 
--- shuup/front/settings.py
+++ shuup/front/settings.py
@@ -6,11 +6,11 @@
 # LICENSE file in the root directory of this source tree.
 
 
-#: This is used to get the login form for the login dropdown in navigation.jinja
+#: This is used to get the login form for the login dropdown in navigation.jinja file.
 SHUUP_LOGIN_VIEW = (
     "shuup.front.apps.auth.views:LoginView")
 
-#: Spec string for the class used for creating Order from a Basket.
+#: Spec string for the class used to create Order from a Basket.
 #:
 #: This is the easiest way to customize the order creation process
 #: without having to override a single URL or touch the ``shuup.front`` code.
@@ -23,29 +23,29 @@ SHUUP_BASKET_ORDER_CREATOR_SPEC = (
 SHUUP_BASKET_VIEW_SPEC = (
     "shuup.front.views.basket:DefaultBasketView")
 
-#: Spec string for the command dispatcher used when products are added/deleted/etc.
+#: Spec string for the command dispatcher. Used when products are added/deleted/etc
 #: from the basket.
 #:
 #: This view deals with commands ``POST``ed to ``/basket/``.
 SHUUP_BASKET_COMMAND_DISPATCHER_SPEC = (
     "shuup.core.basket.command_dispatcher:BasketCommandDispatcher")
 
-#: Spec string for the update method dispatcher used when the basket is updated (usually
+#: Spec string for the update method dispatcher. Used when the basket is updated (usually
 #: on the basket page).
 SHUUP_BASKET_UPDATE_METHODS_SPEC = (
     "shuup.front.basket.update_methods:BasketUpdateMethods")
 
-#: Spec string for the basket class used in the frontend.
+#: Spec string for the basket class, used in the frontend.
 #:
 #: This is used to customize the behavior of the basket for a given installation,
-#: for instance to modify prices of products based on certain conditions, etc.
+#: for instance to modify prices of products based on certain condition.
 SHUUP_BASKET_CLASS_SPEC = (
     "shuup.front.basket.objects:BaseBasket")
 
-#: The spec string defining which basket storage class to use for the frontend.
+#: The spec string, defining which basket storage class to use for the frontend.
 #:
-#: Basket storages are responsible for persisting visitor basket state,
-#: the default stores the basket to database (DatabaseBasketStorage)
+#: Basket storages are responsible for persisting visitor basket state.
+#: The default stores the basket to database (`DatabaseBasketStorage`).
 #: Custom storage backends could use caches, flat files, etc. if required.
 SHUUP_BASKET_STORAGE_CLASS_SPEC = (
     "shuup.front.basket.storage:DatabaseBasketStorage")
@@ -57,9 +57,9 @@ SHUUP_BASKET_STORAGE_CLASS_SPEC = (
 SHUUP_CHECKOUT_VIEW_SPEC = (
     "shuup.front.views.checkout:DefaultCheckoutView")
 
-#: Default product lists facet configuration
+#: Default product lists facet configuration.
 #:
-#: This configuration will be used if the configuration is not set from admin
+#: This configuration will be used if the configuration is not set from admin.
 SHUUP_FRONT_DEFAULT_SORT_CONFIGURATION = {
     "sort_products_by_name": True,
     "sort_products_by_name_ordering": 1,
@@ -67,44 +67,43 @@ SHUUP_FRONT_DEFAULT_SORT_CONFIGURATION = {
     "sort_products_by_price_ordering": 2
 }
 
-#: Default cache duration for template helpers
+#: Default cache duration for template helpers (in seconds).
 #:
-#: Cache duration in seconds for front template helpers. Default 30 minutes.
+#: Cache duration in seconds for front template helpers. Default: 30 minutes.
 SHUUP_TEMPLATE_HELPERS_CACHE_DURATION = 60*30
 
-#: A dictionary defining properties to override the default field properties of the
-#: person contact form. Should map the field name (as a string) to a dictionary
-#: containing the overriding Django form field properties, as in the following
-#: example which makes the gender field hidden:
+#: A dictionary, defining properties to override the default field properties of the
+#: person contact form. Should map the field name (as a string) to a dictionary.
+#: The dictionary should contain the overriding Django form field properties, as in
+#: the following example (makes the `gender` field hidden):
 #:
 #: SHUUP_PERSON_CONTACT_FIELD_PROPERTIES = {
 #:    "gender": {"widget": forms.HiddenInput()}
 #: }
 #:
-#: It should be noted, however, that overriding some settings (such as making a
+#: It should be noted, however, that overriding some of the settings (such as making a
 #: required field non-required) could create other validation issues.
 SHUUP_PERSON_CONTACT_FIELD_PROPERTIES = {}
 
 #: A dictionary defining properties to override the default field properties of the
-#: confirm form. Should map the field name (as a string) to a dictionary
-#: containing the overriding Django form field properties, as in the following
-#: example which makes the gender field hidden:
+#: `confirm` form. Should map the field name (as a string) to a dictionary.
+#: The dictionary should contain the overriding Django form field properties, as in
+#: the following example (makes the `gender` field hidden):
 #:
 #: SHUUP_CHECKOUT_CONFIRM_FORM_PROPERTIES = {
 #:    "marketing": {"initial": True},
 #:    "comment": {"widget": forms.HiddenInput()}
 #: }
 #:
-#: It should be noted, however, that overriding some settings (such as making a
+#: It should be noted, however, that overriding some of the settings (such as making a
 #: required field non-required) could create other validation issues.
 SHUUP_CHECKOUT_CONFIRM_FORM_PROPERTIES = {}
 
-#: Shuup powered by default content. This content is rendered in theme bottom
-#: by default at shuup.front.templates.shuup.front.macros.footer.jinja
+#: The default "Shuup powered by" content. This content is rendered in theme bottom
+#: by default at `shuup.front.templates.shuup.front.macros.footer.jinja`.
 SHUUP_FRONT_POWERED_BY_CONTENT = """
     <p class="powered">Powered by <a target="_blank" href="https://shuup.com">Shuup</a></p>
 """.strip()
-
 
 #: Override sort and filters labels with your own.
 #:
@@ -118,7 +117,7 @@ SHUUP_FRONT_POWERED_BY_CONTENT = """
 SHUUP_FRONT_OVERRIDE_SORTS_AND_FILTERS_LABELS_LOGIC = {}
 
 
-#: Maximum allowed file size (bytes) for uploads when posting to
-#: `shuup.front.views.upload.media_upload`
+#: Maximum allowed file size (in bytes) for uploads, when posting to
+#: `shuup.front.views.upload.media_upload`.
 #:
 SHUUP_FRONT_MAX_UPLOAD_SIZE = 500000
