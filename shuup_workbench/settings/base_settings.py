6fb4c917ecf96e15d9cc6b4c6478ada5da3b1ffc
Eero Ruohola <eero@ruohola.dev>
2020-07-03 18:57:06 +0300

    Revert "Change sqlite db location"

--- shuup_workbench/settings/base_settings.py
+++ shuup_workbench/settings/base_settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'shuup_workbench.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../.sqlite/db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
