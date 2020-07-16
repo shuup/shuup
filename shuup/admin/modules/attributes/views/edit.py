b68ed6425bae82416befac9426d5b8b5530c8296
Panu Tulimäki <panu@shuup.com>
2020-07-16 13:36:44 +0300

    Add option to delete attributes

--- shuup/admin/modules/attributes/views/edit.py
+++ shuup/admin/modules/attributes/views/edit.py
@@ -7,6 +7,11 @@
 # LICENSE file in the root directory of this source tree.
 from __future__ import unicode_literals
 
+from django.contrib import messages
+from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.utils.encoding import force_text
+from django.views.generic import DetailView
 from shuup.admin.form_modifier import ModifiableFormMixin, ModifiableViewMixin
 from shuup.admin.utils.views import CreateOrUpdateView
 from shuup.core.models import Attribute
@@ -26,3 +31,14 @@ class AttributeEditView(ModifiableViewMixin, CreateOrUpdateView):
     form_class = AttributeForm
     template_name = "shuup/admin/attributes/edit.jinja"
     context_object_name = "attribute"
+
+
+class AttributeDeleteView(DetailView):
+    model = Attribute
+
+    def post(self, request, *args, **kwargs):
+        attribute = self.get_object()
+        attribute_name = force_text(attribute)
+        attribute.delete()
+        messages.success(request, _("%s has been deleted.") % attribute_name)
+        return HttpResponseRedirect(reverse_lazy("shuup_admin:attribute.list"))
