0583de9b40cc1673c171cd4da15e51d8b4159b07
William <wilindho@gmail.com>
2020-08-12 11:56:39 +0300

    Add media folder support for the visibility and owner in admin
    - Added so admin and other users with edit-access permissions will be able to change if a folder is
    visible for all vendors and which users own the folder
    
    - Changes so the query to get the folders is accurate with the visibility and owning of folders

---
+++ shuup/admin/modules/media/form_parts.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# This file is part of Shuup.
+#
+# Copyright (c) 2012-2020, Shoop Commerce Ltd. All rights reserved.
+#
+# This source code is licensed under the OSL-3.0 license found in the
+# LICENSE file in the root directory of this source tree
+
+from shuup.admin.form_part import FormPart, TemplatedFormDef
+from shuup.admin.modules.media.forms import MediaFolderForm
+
+
+class MediaFolderBaseFormPart(FormPart):
+    priority = 1
+
+    def get_form_defs(self):
+        yield TemplatedFormDef(
+            "media_form",
+            MediaFolderForm,
+            template_name="shuup/admin/media/edit_folder.jinja",
+            required=False,
+            kwargs={
+                "instance": self.object,
+            }
+        )
+
+    def form_valid(self, form):
+        self.object = form["media_form"].save()
