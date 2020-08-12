0706f842405e16547b52ce63301548fd0c19ea26
William <wilindho@gmail.com>
2020-08-12 11:45:17 +0300

    Add owner and visibility to media folder
    - Added boolean visibility field to media folder so admin can select if the folder is visible for everyone
    - Added m2m owner filed so a folder can have users as owners
    
    This is so all actions can be preformed on folders that are in the sub folder tree

--- shuup/core/models/_media.py
+++ shuup/core/models/_media.py
@@ -7,6 +7,7 @@
 # LICENSE file in the root directory of this source tree.
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
@@ -30,7 +31,25 @@ class MediaFolder(models.Model):
     folder = FilerFolderField(related_name="media_folder", verbose_name=_("folder"), on_delete=models.CASCADE)
     shops = models.ManyToManyField(
         "shuup.Shop", related_name="media_folders", verbose_name=_('shops'),
-        help_text=_("Select which shops you would like the folder to be visible in."))
+        help_text=_("Select which shops you would like the folder to be visible in.")
+    )
+
+    visible = models.BooleanField(
+        default=False, verbose_name=_("visible"),
+        help_text=_("Should this folder be visible for everyone in the media browser")
+    )
+
+    owners = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name=_("owning_media_folders"), verbose_name=_('owners'),
+        help_text=_("Select which users will own this folder.")
+    )
 
     def __str__(self):  # pragma: no cover
         return "%s" % (self.folder)
+
+    def get_all_children(self):
+        folders = [self.folder]
+        for sub_folder in self.folder.children.all():
+            for sub_media_folder in sub_folder.media_folder.all():
+                folders.extend(sub_media_folder.get_all_children())
+        return folders
