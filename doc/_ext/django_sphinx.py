e2ef42984882dfd9a4e031515423435114c5d248
Panu Tulimäki <panu@shuup.com>
2020-10-19 13:09:06 +0300

    Docs: fix build error

--- doc/_ext/django_sphinx.py
+++ doc/_ext/django_sphinx.py
@@ -65,13 +65,13 @@ def _process_field_help_text_and_verbose_name(field, lines, is_translation=0):
 
 def _process_field_type(field, lines, latelines):
     if isinstance(field, models.ForeignKey):
-        to = _resolve_field_destination(field, field.remote_field.target_field)
+        to = _resolve_field_destination(field, field.remote_field.model)
 
         lines.append(':type %s: %s to :class:`%s.%s`' % (
             field.attname,
             type(field).__name__, to.__module__, to.__name__))
     elif isinstance(field, models.ManyToManyField):
-        to = _resolve_field_destination(field, field.remote_field.target_field)
+        to = _resolve_field_destination(field, field.remote_field.model)
 
         lines.append(':type %s: %s to :class:`%s.%s`' % (
             field.attname,
