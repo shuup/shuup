e70f6790b929c7f251e6935ed6adff7c9e8c04c8
Panu Tulimäki <panu@shuup.com>
2020-12-15 12:21:35 +0200

    Xtheme: cache placeholder rendering
    This requires that plugins used in placholders are static
    meaning that if you have some content that depends on current user
    then you would need to implement such a plugins fetch the content
    asynchronously.
    
    Create cache key from view_config pk, language, placholder name and
    layout data key. This way whenever the view_config is updated or touched
    the cache gets invalidated.

---
+++ shuup/xtheme/cache.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# This file is part of Shuup.
+#
+# Copyright (c) 2012-2020, Shoop Commerce Ltd. All rights reserved.
+#
+# This source code is licensed under the OSL-3.0 license found in the
+# LICENSE file in the root directory of this source tree.
+from django.utils.timezone import now
+
+from .models import SavedViewConfig, SavedViewConfigStatus
+
+
+def bump_xtheme_cache(*args, **kwargs):
+    SavedViewConfig.objects.filter(
+        status=SavedViewConfigStatus.PUBLIC
+    ).update(
+        modified_on=now()
+    )
