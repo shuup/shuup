75682ad743a0546f22596b5ed8bd30768c652605
Panu Tulimäki <panu@shuup.com>
2021-01-06 11:26:05 +0200

    Admin: tune admin shop provider cached shop

--- shuup/admin/shop_provider.py
+++ shuup/admin/shop_provider.py
@@ -63,6 +63,7 @@ class AdminShopProvider(object):
             # only can set if the user is superuser or is the shop staff
             if shop.staff_members.filter(pk=request.user.pk).exists() or request.user.is_superuser:
                 request.session[SHOP_SESSION_KEY] = shop.id
+                request._cached_admin_shop = shop
             else:
                 raise PermissionDenied(_("You must have the Access to Admin Panel permissions to this shop."))
 
@@ -70,6 +71,7 @@ class AdminShopProvider(object):
             self.unset_shop(request)
 
     def unset_shop(self, request):
+        request._cached_admin_shop = None
         if SHOP_SESSION_KEY in request.session:
             del request.session[SHOP_SESSION_KEY]
 
