a0fa758295148e08be8fbcb5ada5b29647141c3d
Christian Hess <christianhess.rlz@gmail.com>
2021-01-19 17:04:50 -0300

    Add unit test for basket command middleware

---
+++ shuup/testing/basket_middleware.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# This file is part of Shuup.
+#
+# Copyright (c) 2012-2020, Shoop Commerce Ltd. All rights reserved.
+#
+# This source code is licensed under the OSL-3.0 license found in the
+# LICENSE file in the root directory of this source tree.
+from shuup.core.basket.command_middleware import BaseBasketCommandMiddleware
+
+
+class TestBasketCommandMiddleware(BaseBasketCommandMiddleware):
+    def preprocess_kwargs(self, basket, request, command: str, kwargs: dict) -> dict:
+        kwargs["extra"] = kwargs.get("extra") or {}
+        kwargs["extra"]["extra"] = kwargs["extra"].get("extra") or {}
+        kwargs["extra"]["extra"]["line_options"] = "works"
+        return kwargs
+
+    def postprocess_response(self, basket, request, command: str, kwargs: dict, response: dict) -> dict:
+        response["it_works"] = True
+        return response
