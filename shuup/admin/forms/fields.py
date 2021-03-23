f49a723a96f47377bae0e1c2cb57235100ca8d8b
nvthanh11 <nvthanh11@cmc.com.vn>
2021-03-23 14:13:27 +0700

    Vendor can see hidden categories RM-11

--- shuup/admin/forms/fields.py
+++ shuup/admin/forms/fields.py
@@ -66,8 +66,10 @@ class Select2ModelField(Field):
 class Select2MultipleField(Field):
     widget = SelectMultiple
 
-    def __init__(self, model, *args, **kwargs):
+    def __init__(self, model, search_mode=None, *args, **kwargs):
         self.model = model
+        if search_mode:
+            self.search_mode = search_mode
         super(Select2MultipleField, self).__init__(*args, **kwargs)
 
     def prepare_value(self, value):
@@ -91,8 +93,11 @@ class Select2MultipleField(Field):
         attrs = super(Select2MultipleField, self).widget_attrs(widget)
         model_name = "%s.%s" % (self.model._meta.app_label, self.model._meta.model_name)
         attrs.update({"data-model": model_name})
+        if getattr(self, "search_mode", None):
+            attrs.update({"search-mode": self.search_mode})
         if not self.required:
             attrs["data-allow-clear"] = "true"
+        print(attrs)
         return attrs
 
 
