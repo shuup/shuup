1a9c09ad1bcda8feb287bf5c6665ba0578c14be0
Christian Hess <christianhess.rlz@gmail.com>
2021-05-23 22:37:54 -0300

    Fix makemessages management command

--- shuup/core/management/commands/makemessages.py
+++ shuup/core/management/commands/makemessages.py
@@ -10,14 +10,12 @@ Patched version of Django's Makemessages that works with Jinja2.
 Works by monkey patching django.utils.translation.trans_real.templatize
 with our version.
 """
-
 import datetime
 import os
-from io import BytesIO
+from io import BytesIO, StringIO
 
 import babel.messages.extract
 from django.core.management.commands import makemessages
-from django.utils.six import StringIO
 from django.utils.translation import template as trans_real
 
 KEYWORDS = dict(
