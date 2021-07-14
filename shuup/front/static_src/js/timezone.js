4c8846c84f4c4894700a72d739ab41f447da5837
Christian Hess <christianhess.rlz@gmail.com>
2021-07-14 10:45:46 -0300

    Front: add timezone view to save the user's current timezone

---
+++ shuup/front/static_src/js/timezone.js
@@ -0,0 +1,10 @@
+/**
+ * This file is part of Shuup.
+ *
+ * Copyright (c) 2012-2021, Shuup Commerce Inc. All rights reserved.
+ *
+ * This source code is licensed under the OSL-3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import moment from "moment-timezone";
+window.moment = moment;
