51c4082007513b91573877cad58ff04870cc787f
Emiliano Beronich <emiliano.beronich@shuup.com>
2021-07-14 10:10:02 -0300

    Fix isort issue.

--- shuup/admin/modules/products/validators.py
+++ shuup/admin/modules/products/validators.py
@@ -6,6 +6,7 @@
 # This source code is licensed under the OSL-3.0 license found in the
 # LICENSE file in the root directory of this source tree.
 from typing import Iterable
+
 from shuup.admin.modules.products.issues import ProductValidationIssue
 from shuup.core.models import Shop, ShopProduct, Supplier
 
