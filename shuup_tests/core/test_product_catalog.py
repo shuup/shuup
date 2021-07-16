524e3fff15712772ea1ec02d45655a7a12112268
Christian Hess <christianhess.rlz@gmail.com>
2021-07-16 17:56:10 -0300

    Implement the product indexing in the CGP module

--- shuup_tests/core/test_product_catalog.py
+++ shuup_tests/core/test_product_catalog.py
@@ -78,3 +78,44 @@ def test_product_catalog_purchasable():
     assert shop_products_qs.count() == 1
     for index, value in enumerate(values):
         assert value == expected_prices[index]
+
+
+def test_product_catalog_variations():
+    shop = factories.get_default_shop()
+    supplier = factories.get_default_supplier()
+    parent = factories.create_product("p1", shop=shop, supplier=supplier, default_price=Decimal("10"))
+    child1 = factories.create_product("p2", shop=shop, supplier=supplier, default_price=Decimal("20"))
+    child2 = factories.create_product("p3", shop=shop, supplier=supplier, default_price=Decimal("40"))
+    child3 = factories.create_product("p4", shop=shop, supplier=supplier, default_price=Decimal("50"))
+
+    child1.link_to_parent(parent)
+    child2.link_to_parent(parent)
+    child3.link_to_parent(parent)
+
+    catalog = ProductCatalog(context=ProductCatalogContext(purchasable_only=False))
+    ProductCatalog.index_product(parent)
+
+    # return a Product queryset annotated with price and discounted price
+    products_qs = catalog.get_products_queryset().order_by("catalog_price")
+    expected_prices = [
+        (child1.pk, Decimal("20"), None),
+        (child2.pk, Decimal("40"), None),
+        (child3.pk, Decimal("50"), None),
+    ]
+    values = products_qs.values_list("pk", "catalog_price", "catalog_discounted_price")
+    assert products_qs.count() == 3
+    for index, value in enumerate(values):
+        assert value == expected_prices[index]
+
+    # return a ShopProduct queryset
+    expected_prices = [
+        (parent.get_shop_instance(shop).pk, Decimal("10"), None),
+        (child1.get_shop_instance(shop).pk, Decimal("20"), None),
+        (child2.get_shop_instance(shop).pk, Decimal("40"), None),
+        (child3.get_shop_instance(shop).pk, Decimal("50"), None),
+    ]
+    shop_products_qs = catalog.get_shop_products_queryset().order_by("catalog_price")
+    values = shop_products_qs.values_list("pk", "catalog_price", "catalog_discounted_price")
+    assert shop_products_qs.count() == 4
+    for index, value in enumerate(values):
+        assert value == expected_prices[index]
