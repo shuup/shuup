944967fde769eeb52875ba8ad88c679ff11e7480
Christian Hess <christianhess.rlz@gmail.com>
2021-07-22 19:27:32 -0300

    Add visibility attribute on the catalog context
    Make it possible to return only visible products when purchasibility is False

--- shuup/core/catalog/__init__.py
+++ shuup/core/catalog/__init__.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import AbstractUser, AnonymousUser
 from django.db.models import Case, F, OuterRef, Q, Subquery, When
 from django.db.models.functions import Coalesce
 from django.db.models.query import QuerySet
+from django.utils import timezone
 from typing import Optional, Union
 
 from shuup.core.catalog.signals import index_catalog_shop_product
@@ -18,11 +19,14 @@ from shuup.core.models import (
     Product,
     ProductCatalogPrice,
     ProductMode,
+    ProductVisibility,
     Shop,
     ShopProduct,
+    ShopProductVisibility,
     Supplier,
 )
 from shuup.core.pricing import get_discount_modules, get_pricing_module
+from shuup.core.utils.users import is_user_all_seeing
 
 
 class ProductCatalogContext:
@@ -39,10 +43,18 @@ class ProductCatalogContext:
     `user` can be either a User instance or a user id,
         used to filter the products for the given user.
 
-    `contact` can be either a Contact instance or a contact id,
-        used to filter the products for the given contact.
+    `contact` a Contact instance used to filter the products for the given contact.
+
+    `visible_only` filter the products that are visible.
+        Deleted products and with visibility limitations
+        will be removed from the list.
+
+    `visibility` the shop product visibility. If set to None, all
+        products for any visibility will be returned.
+        if `purchasable_only` is True, this will be ignored.
 
     `purchasable_only` filter the products that can be purchased.
+        This will make `visible_only` to be True.
     """
 
     def __init__(
@@ -50,14 +62,18 @@ class ProductCatalogContext:
         shop: Optional[Union[Shop, int]] = None,
         supplier: Optional[Union[Supplier, int]] = None,
         user: Optional[Union[AbstractUser, AnonymousUser, int]] = None,
-        contact: Optional[Union[Contact, int]] = None,
+        contact: Optional[Contact] = None,
+        visible_only: bool = True,
+        visibility: Optional[ShopProductVisibility] = None,
         purchasable_only: bool = True,
     ):
         self.shop = shop
         self.supplier = supplier
         self.user = user
         self.contact = contact
+        self.visible_only = visible_only
         self.purchasable_only = purchasable_only
+        self.visibility = visibility
 
 
 class ProductCatalog:
@@ -76,9 +92,9 @@ class ProductCatalog:
         purchasable_only = self.context.purchasable_only
 
         if shop:
-            filters &= Q(shop_id=shop.pk)
+            filters &= Q(shop=shop)
         if supplier:
-            filters &= Q(supplier_id=supplier.pk)
+            filters &= Q(supplier=supplier)
         if purchasable_only:
             filters &= Q(is_available=True)
 
@@ -87,7 +103,7 @@ class ProductCatalog:
             filters = Q(
                 Q(filters)
                 & Q(
-                    Q(contact_id=contact.pk)
+                    Q(contact=contact)
                     # evaluate contact group to prevent doing expensive joins on db
                     | Q(contact_group_id__in=list(contact.groups.values_list("pk", flat=True)))
                     | Q(contact_group__isnull=True, contact__isnull=True)
@@ -119,10 +135,63 @@ class ProductCatalog:
             .filter(filters)
             .order_by(Coalesce("discounted_price_value", "price_value").asc())
         )
+
+        # only visible products, we need to filter those through queryset
+        if not self.context.purchasable_only and self.context.visible_only:
+            visible_shop_products = self._filter_visible_shop_products(ShopProduct.objects.all())
+            queryset = queryset.filter(
+                pk__in=visible_shop_products.values_list("product_id", flat=True), deleted=False
+            ).distinct()
+
         return queryset.annotate(
             catalog_price=Subquery(product_prices.values("price_value")[:1]),
             catalog_discounted_price=Subquery(product_prices.values("discounted_price_value")[:1]),
         )
+
+    def _filter_visible_shop_products(self, queryset: "QuerySet[ShopProduct]") -> "QuerySet[ShopProduct]":
+        """
+        Filter visible shop products according to the context
+        """
+        contact = self.context.contact
+
+        user = self.context.user
+        if not user and self.context.contact and hasattr(self.context.contact, "user"):
+            user = self.context.contact.user
+
+        user_all_seeing = is_user_all_seeing(user) if user else False
+
+        # user can see all shop products
+        if user_all_seeing:
+            return queryset
+
+        shop_product_filters = Q(Q(available_until__isnull=True) | Q(available_until__gte=timezone.now()))
+
+        if self.context.shop:
+            shop_product_filters &= Q(shop=self.context.shop)
+
+        if contact:
+            shop_product_filters &= Q(
+                Q(visibility_limit__in=(ProductVisibility.VISIBLE_TO_ALL, ProductVisibility.VISIBLE_TO_LOGGED_IN))
+                | Q(visibility_limit=ProductVisibility.VISIBLE_TO_GROUPS, visibility_groups__in=contact.groups.all())
+            )
+        else:
+            shop_product_filters &= Q(
+                Q(visibility_limit=ProductVisibility.VISIBLE_TO_ALL)
+                | Q(
+                    visibility_limit=ProductVisibility.VISIBLE_TO_GROUPS,
+                    visibility_groups=AnonymousContact.get_default_group(),
+                )
+            )
+
+        if self.context.visibility:
+            if self.context.visibility not in (ShopProductVisibility.NOT_VISIBLE, ShopProductVisibility.ALWAYS_VISIBLE):
+                shop_product_filters &= Q(
+                    visibility__in=[self.context.visibility, ShopProductVisibility.ALWAYS_VISIBLE]
+                )
+        else:
+            shop_product_filters &= Q(visibility=ShopProductVisibility.ALWAYS_VISIBLE)
+
+        return queryset.filter(shop_product_filters).distinct()
 
     def get_products_queryset(self) -> "QuerySet[Product]":
         """
@@ -148,6 +217,9 @@ class ProductCatalog:
             .filter(filters)
             .order_by(Coalesce("discounted_price_value", "price_value").asc())
         )
+
+        if not self.context.purchasable_only and self.context.visible_only:
+            queryset = self._filter_visible_shop_products(queryset).filter(product__deleted=False)
 
         return queryset.annotate(
             # as we are filtering ShopProducts, we can fallback to default_price_value
