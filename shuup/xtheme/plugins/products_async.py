2f1f868908eb341e046fdf821b016753d7448777
Christian Hess <christianhess.rlz@gmail.com>
2021-07-22 09:59:09 -0300

    Xtheme: use the new Catalog API

--- shuup/xtheme/plugins/products_async.py
+++ shuup/xtheme/plugins/products_async.py
@@ -10,6 +10,7 @@ from django.urls import reverse
 from django.utils.translation import get_language, ugettext_lazy as _
 from enumfields import Enum
 
+from shuup.core.catalog import ProductCatalog, ProductCatalogContext
 from shuup.core.models import Product, ProductCrossSell, ProductCrossSellType
 from shuup.front.template_helpers.general import (
     get_best_selling_products,
@@ -336,7 +337,15 @@ class ProductSelectionPlugin(TemplatedPlugin):
         products_qs = Product.objects.none()
 
         if request.is_ajax() and products:
-            products_qs = Product.objects.listed(shop=request.shop, customer=request.customer).filter(pk__in=products)
+            catalog = ProductCatalog(
+                ProductCatalogContext(
+                    shop=request.shop,
+                    user=request.user,
+                    contact=getattr(request, "customer", None),
+                    purchasable_only=True,
+                )
+            )
+            products_qs = catalog.get_products_queryset().filter(pk__in=products)
 
         return {
             "request": request,
